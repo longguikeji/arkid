d252316aac6c62b1bc56686a05beb5f2bed3f00e
skoogi <skoogiby@gmail.com>
2019-09-12 19:12:35 +0800

    refactor(tweak): tweak

--- oneid_meta/migrations/0032_auto_20190617_1952.py
+++ oneid_meta/migrations/0032_auto_20190617_1952.py
@@ -11,6 +11,7 @@ def init_user_native_field(apps, schema_editor):
     if settings.TESTING:
         return None
     NativeField = apps.get_model('oneid_meta', 'NativeField')
+    subject = 'user'
 
     NativeField.objects.create(subject=subject, name='职位', key='position', schema={'type': 'string'})
     NativeField.objects.create(subject=subject, name='私人邮箱', key='private_email', schema={'type': 'string'})
