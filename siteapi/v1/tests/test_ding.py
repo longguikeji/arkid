077fb9bcdc1f14689f092999515eb902f3101494
ShiYue <shiyue@longguikeji.com>
2019-10-10 15:50:09 +0800

    refactor(component): 修改测试命名不规范处

--- siteapi/v1/tests/test_ding.py
+++ siteapi/v1/tests/test_ding.py
@@ -67,7 +67,7 @@ class UCenterTestCase(TestCase):
         self.assertEqual(res_keys, expect)
 
     @mock.patch("siteapi.v1.views.sns.DingQrCallbackView.get_ding_id")
-    def test_ding_sns_login_2(self, mock_get_ding_id):
+    def test_ding_sns_login_unregistered(self, mock_get_ding_id):
         client = self.client
         ding_config = DingConfig.get_current()
         ding_config.__dict__.update(qr_app_id='qr_app_id', qr_app_secret='qr_app_secret', qr_app_valid=True)
@@ -81,13 +81,13 @@ class UCenterTestCase(TestCase):
         expect = {'token': '', 'ding_id': 'unregistered_dingid'}
         self.assertEqual(res.json(), expect)
 
-    def test_ding_sns_login_3(self):
+    def test_ding_sns_login_forbidden(self):
         client = self.client
         res = client.post(reverse('siteapi:ding_qr_callback'), data={'code':'CODE...........', 'state':'STATE'})
-        expect = {'token': ''}
-        expect2 = 403
-        self.assertEqual(res.json(), expect)
-        self.assertEqual(res.status_code, expect2)
+        expect_json = {'err_msg':'ding qr not allowed'}
+        expect_code = 403
+        self.assertEqual(res.json(), expect_json)
+        self.assertEqual(res.status_code, expect_code)
 
     @mock.patch('siteapi.v1.serializers.ucenter.SMSClaimSerializer.check_sms_token')
     def test_ding_query_user(self, mock_check_sms_token):
