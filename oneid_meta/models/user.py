9a2471988ce179939affee76f577e9d2f493c8ad
ShiYue <shiyue@longguikeji.com>
2019-11-04 19:00:14 +0800

    fix(dinguser model): 钉钉扫码注册用户不能改名的bug

--- oneid_meta/models/user.py
+++ oneid_meta/models/user.py
@@ -506,8 +506,8 @@ class DingUser(BaseModel):
     钉钉用户
     '''
     user = models.OneToOneField(User, verbose_name='用户', related_name='ding_user', on_delete=models.PROTECT)
-    account = models.CharField(max_length=64, blank=False, verbose_name='钉钉账号(手机)')
-    uid = models.CharField(max_length=255, blank=False, verbose_name='员工在企业内的唯一标识')
+    account = models.CharField(max_length=64, blank=True, verbose_name='钉钉账号(手机)')
+    uid = models.CharField(max_length=255, blank=True, verbose_name='员工在企业内的唯一标识')
     data = models.TextField(blank=True, default='{}', verbose_name='钉钉员工详细数据(JSON)')
     ding_id = models.TextField(max_length=255, blank=True, verbose_name='钉钉ID')
     open_id = models.TextField(max_length=255, blank=True, verbose_name='用户在当前开放应用内的唯一标识')
