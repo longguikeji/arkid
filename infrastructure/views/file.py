d75da14132dcf232b5b76453a0df58ad16d09897
ShiYue <shiyue@longguikeji.com>
2019-11-16 14:18:41 +0800

    improvement(file): 增加文件本地存储

--- infrastructure/views/file.py
+++ infrastructure/views/file.py
@@ -7,10 +7,10 @@ import uuid as uuid_utils
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
 from django.conf import settings
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, HttpResponse
 from django.views.generic import View
-
 from common.minio_utils import (
     put_object,
     presign_get,
@@ -32,12 +32,16 @@ class FileCreateAPIView(generics.CreateAPIView):
         uuid = uuid_utils.uuid4().hex
         suffix = os.path.splitext(file.name)[1]
         file_name = uuid + suffix
-        put_object(
-            bucket_name=settings.MINIO_BUCKET,
-            object_name=file_name,
-            file_data=file,
-            length=file.size,
-        )
+        try:
+            put_object(
+                bucket_name=settings.MINIO_BUCKET,
+                object_name=file_name,
+                file_data=file,
+                length=file.size,
+            )
+        except Exception:    # pylint: disable=broad-except
+            with open(settings.UPLOADFILES_PATH + file_name, 'wb') as f:
+                f.write(file.read())
         return Response({'file_name': file_name})
 
 
@@ -53,10 +57,16 @@ class FileAPIView(View):
         '''
         download file
         '''
-        url = presign_get(
-            bucket_name=settings.MINIO_BUCKET,
-            object_name=uuid,
-            response_headers={
-                'response-content-disposition': 'attachment;filename=%s' % uuid,
-            })
-        return HttpResponseRedirect(url)
+        try:
+            url = presign_get(bucket_name=settings.MINIO_BUCKET,
+                              object_name=uuid,
+                              response_headers={
+                                  'response-content-disposition': 'attachment;filename=%s' % uuid,
+                              })
+            return HttpResponseRedirect(url)
+        except Exception:    # pylint: disable=broad-except
+            data = ''
+            if os.path.exists(settings.UPLOADFILES_PATH + uuid):
+                data = open(settings.UPLOADFILES_PATH + uuid, 'rb').read()
+                return HttpResponse(data)
+            return Response(data, HTTP_404_NOT_FOUND)
