c0ecba860df13980de49868495d6196039cec9ac
雷韩 <leihan@leihandeiMac.local>
2019-11-26 17:17:00 +0800

    fix: 设置默认method

--- oneid_meta/migrations/0066_minioconfig_storageconfig.py
+++ oneid_meta/migrations/0066_minioconfig_storageconfig.py
@@ -26,6 +26,10 @@ def init_storage_config(apps, schema_editor):
         minio_config.bucket = getattr(settings, 'MINIO_BUCKET', '')
         minio_config.save()
 
+        if not (minio_config.access_key == '' or minio_config.secret_key == ''):
+            storage_config.method = 'minio'
+            storage_config.save()
+
 
 class Migration(migrations.Migration):
 
