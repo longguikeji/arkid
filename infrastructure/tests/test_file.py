b95f96b45897e2c47229fd0e19c90067fe424d03
shiyueLongguikeji <shiyue@longguikeji.com>
2019-12-20 19:15:23 +0800

    fix(file download): response status

--- infrastructure/tests/test_file.py
+++ infrastructure/tests/test_file.py
@@ -2,14 +2,15 @@
 test for file
 '''
 from django.urls import reverse
+from rest_framework.status import HTTP_404_NOT_FOUND
 from siteapi.v1.tests import TestCase
 
 
-class BugTestCase(TestCase):
-    '''bug修复测试
+class FileTestCase(TestCase):
+    '''测试文件存储
     '''
     def test_download_file(self):
-        '''请求图片失败返回404
+        '''请求图片失败返回
         '''
         res = self.client.get(reverse('infra:download_file', args=('fjdkslaf.png', )))
-        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.status_code, HTTP_404_NOT_FOUND)
