8712adee77109275360e06fdb52ed058ac93186a
meixinyue <meixinyue11@163.com>
2020-03-18 21:31:13 +0800

    更新

--- test/e2e/src/actions/organization.ts
+++ test/e2e/src/actions/organization.ts
@@ -1,30 +1,30 @@
 import {Page, launch} from 'puppeteer';
     export class organizationAction{
-        public async origanization(page:Page, searchOrg:string){
+        public async origanization(page:Page){
             
             const orgBtn = await page.waitForSelector('a[href="#/workspace/contacts"]');
             await orgBtn.click();
             page.waitFor(3000);
 
-            const groups = await page.$$('.lg-layout--body .ui-contact-page--side>li');
+            // const groups = await page.$$('.lg-layout--body .ui-contact-page--side>li');
             
-            for (let i = 1; i < 5; i++) {
-                const button = groups[i]
+            // for (let i = 1; i < 5; i++) {
+            //     const button = groups[i]
 
-                await button.click();
-                const orgInput = await page.waitForSelector('.ivu-input.ivu-input-default.ivu-input-with-suffix');
-                await orgInput.type(searchOrg);
+            //     await button.click();
+            //     const orgInput = await page.waitForSelector('.ivu-input.ivu-input-default.ivu-input-with-suffix');
+            //     await orgInput.type(searchOrg);
 
-            }
+            // }
 
-            for (let i = 6; i < groups.length; i++) {
-                const button = groups[i]
+            // for (let i = 6; i < groups.length; i++) {
+            //     const button = groups[i]
 
-                await button.click();
-                const orgInput = await page.waitForSelector('.ivu-input.ivu-input-default.ivu-input-with-suffix');
-                await orgInput.type(searchOrg);
+            //     await button.click();
+            //     const orgInput = await page.waitForSelector('.ivu-input.ivu-input-default.ivu-input-with-suffix');
+            //     await orgInput.type(searchOrg);
                 
-            }
+            // }
 
         } 
     }
