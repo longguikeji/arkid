d51055574bc9c34d042e3ef40adf0abc2ef0706d
meixinyue <meixinyue11@163.com>
2020-04-11 21:25:58 +0800

    个人资料action方法

--- test/e2e/src/actions/setting.ts
+++ test/e2e/src/actions/setting.ts
@@ -9,23 +9,6 @@ import {Page, launch} from 'puppeteer';
             await personBtn.click();
 
             page.waitFor(3000);
-            // const nameInput = await page.waitForSelector('input[placeholder="请输入 姓名"]');
-            // await nameInput.type(name);
-
-            // const addMobileBtn = await page.waitForSelector('.mobile .ivu-btn.ivu-btn-default');
-            // await addMobileBtn.click();
-
-            // const cancelBtn = await page.waitForSelector('.ivu-modal-confirm-footer>button:first-child');
-            // await cancelBtn.click();
-
-            // const addEmailBtn = await page.waitForSelector('.email .ivu-btn.ivu-btn-default');
-            // await addEmailBtn.click();
-
-            // await cancelBtn.click();
-
-            // const saveBtn = await page.waitForSelector('.flex-row.flex-auto .ivu-btn.ivu-btn-primary');
-            // await saveBtn.click();
-
 
         }
     }
