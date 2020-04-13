d7ef1c0ed8a1554c6bcd40205f8d5a63e9f4892c
skoogi <skoogiby@gmail.com>
2020-04-13 18:13:26 +0800

    test(ui): reload data when setup

--- test/e2e/puppeteer_enviroment.js
+++ test/e2e/puppeteer_enviroment.js
@@ -1,7 +1,26 @@
+const exec = require('child_process').exec
+
+const syncDataCmd = 'docker cp testdata.sql arkid-db:/'
+const loadDataCmd = 'docker exec arkid-db bash -c "mysql -u root -proot arkid < /testdata.sql"'
+
 const PuppeteerEnviromenent = require('jest-environment-puppeteer');
 class CustomEnvironmemnt extends PuppeteerEnviromenent {
     async setup() {
         await super.setup();
+        exec(
+            syncDataCmd, (error, stdout, stderr) => {
+                if (error != null){
+                    throw(error)
+                }
+            },
+        )
+        exec(
+            loadDataCmd, (error, stdout, stderr) =>{
+                if (error != null) {
+                    throw(error)
+                } 
+            }
+        )
     }
 
     async teardown() {
