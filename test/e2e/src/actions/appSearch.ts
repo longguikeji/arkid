9bbbd9a4d4ce8ecf5dd754830ac1eccf93bbe92f
meixinyue <meixinyue11@163.com>
2020-04-21 16:35:34 +0800

    工作台搜索应用action方法

--- test/e2e/src/actions/appSearch.ts
+++ test/e2e/src/actions/appSearch.ts
@@ -2,11 +2,7 @@ import {Page, launch} from 'puppeteer';
 
 export class appSearchAction{
     public async appinformation(page:Page,searchName:string){
-       // const returnBtn = await page.waitForSelector('body > div.lg-layout > header > div.header-right > a:nth-child(1) > button');
-       // await returnBtn.click();
 
-       // const deskBtn = await page.waitForSelector('a[href="#/workspace/apps"]');
-       // deskBtn.click();
         await page.waitFor(2000);
 
         const appInput = await page.waitForSelector('.ivu-input.ivu-input-default.ivu-input-with-suffix');
