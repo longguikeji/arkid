4dcebd059d54b7603a6de8714bda64785d13f93d
meixinyue <meixinyue11@163.com>
2020-04-21 16:36:54 +0800

    工作台组织架构action方法

--- test/e2e/src/actions/organization.ts
+++ test/e2e/src/actions/organization.ts
@@ -2,8 +2,6 @@ import {Page, launch} from 'puppeteer';
 
 export class organizationAction{
     public async origanization(page:Page){
-       // const returnBtn = await page.waitForSelector('body > div.lg-layout > header > div.header-right > a:nth-child(1) > button');
-       // await returnBtn.click();
             
         const orgBtn = await page.waitForSelector('a[href="#/workspace/contacts"]');
         await orgBtn.click();
