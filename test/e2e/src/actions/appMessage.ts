73edaaab57f59d1d3d3acd5a9d27fab58cce1f1b
meixinyue <meixinyue11@163.com>
2020-04-21 16:34:18 +0800

    应用信息action方法

--- test/e2e/src/actions/appMessage.ts
+++ test/e2e/src/actions/appMessage.ts
@@ -2,11 +2,7 @@ import {Page, launch} from 'puppeteer';
 
 export class appMessageAction{
     public async appinformation(page:Page){
-       // const returnBtn = await page.waitForSelector('body > div.lg-layout > header > div.header-right > a:nth-child(1) > button');
-       // await returnBtn.click();
-
-        //const deskBtn = await page.waitForSelector('a[href="#/workspace/apps"]');
-       // deskBtn.click();
+        
         await page.waitFor(3000);
         await page.waitForSelector('.card-list.flex-row>li');
         const apps = await page.$$('.card-list.flex-row>li');
