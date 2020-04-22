947c128295ff81d4286a325f48b3e29597301c2f
skoogi <skoogiby@gmail.com>
2020-04-22 16:39:03 +0800

    refactor(plugin): refactor plugins

--- siteapi/v1/tests/test_plugin.py
+++ siteapi/v1/tests/test_plugin.py
@@ -8,18 +8,14 @@ from siteapi.v1.tests import TestCase
 class CrontabPluginTestCase(TestCase):
     def test_crontab_plugin_list(self):
         res = self.client.get(reverse('siteapi:crontab_plugin_list'))
-        expect = [{
-            'is_active': False,
-            'import_path': 'plugins.crontab.demo.maintain_admin_name_plugin',
-            'schedule': ''
-        }]
+        expect = [{'is_active': False, 'import_path': 'plugins.crontab.tasks.helloword_plugin', 'schedule': ''}]
         self.assertEqual(expect, [self.extract(item, ['is_active', 'import_path', 'schedule']) for item in res.json()])
 
     def test_crontab_plugin_detail(self):
         uuid = self.client.get(reverse('siteapi:crontab_plugin_list')).json()[0]['uuid']
         url = reverse('siteapi:crontab_plugin_detail', args=(uuid, ))
         res = self.client.get(url)
-        expect = {'is_active': False, 'import_path': 'plugins.crontab.demo.maintain_admin_name_plugin', 'schedule': ''}
+        expect = {'is_active': False, 'import_path': 'plugins.crontab.tasks.helloword_plugin', 'schedule': ''}
         self.assertEqualScoped(expect, res.json(), keys=['is_active', 'import_path', 'schedule'])
 
         res = self.client.json_patch(url, data={
@@ -50,6 +46,7 @@ class CrontabPluginTestCase(TestCase):
 class MiddlewarePluginTestCase(TestCase):
     def test_middleware_plugin_list(self):
         res = self.client.get(reverse('siteapi:middleware_plugin_list'))
+        print(res.json())
         expect = [{'is_active': False, 'import_path': 'plugins.middleware.demo.localhostonly_plugin', 'order_no': 0}]
         self.assertEqual(expect, [self.extract(item, ['is_active', 'import_path', 'order_no']) for item in res.json()])
 
