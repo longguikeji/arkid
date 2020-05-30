3d30182fe8cfc9961a3380560de206dd2e73e62e
skoogi <skoogiby@gmail.com>
2020-05-30 13:17:05 +0800

    fix(oauth): fix oauth validate_authorization_request

--- oauth2_provider/oauth2_backends.py
+++ oauth2_provider/oauth2_backends.py
@@ -87,8 +87,8 @@ class OAuthLibCore(object):
             scopes, credentials = self.server.validate_authorization_request(
                 uri, http_method=http_method, body=body, headers=headers)  # oauthlib.oauth2.rfc6749.endpoints.pre_configured.Server -> oauthlib.oauth2.rfc6749.endpoints.authorization.AuthorizationEndpoint -> oauthlib.oauth2.rfc6749.grant_types.authorization_code.AuthorizationCodeGrant
             credentials = self.additional_authorization_for_oidc('credentials', scopes=scopes, credentials=credentials,
-                                                                 nonce=nonce[0], code_challenge=code_challenge[0],
-                                                                 code_challenge_method=code_challenge_method[0])
+                                                                 nonce=nonce, code_challenge=code_challenge,
+                                                                 code_challenge_method=code_challenge_method)
             return scopes, credentials
         except oauth2.FatalClientError as error:
             raise FatalClientError(error=error)
@@ -101,8 +101,8 @@ class OAuthLibCore(object):
             queryset = parse_qs(urlparse(kwargs['uri']).query, keep_blank_values=True, strict_parsing=True)
 
             if 'openid' not in queryset.get('scope', []):
-                return kwargs['uri'],queryset.get('nonce', ''),\
-                   queryset.get('code_challenge', ''), queryset.get('code_challenge_method', '')
+                return kwargs['uri'], queryset.get('nonce', [''])[0],\
+                   queryset.get('code_challenge', [''])[0], queryset.get('code_challenge_method', [''])[0]
 
             queryset['response_type'] = oauth2_settings.UP_COMPATIBLE_OIDC_RESPONSE_TYPE.get(
                 queryset['response_type'][0], None).split()
@@ -111,8 +111,8 @@ class OAuthLibCore(object):
             for key, value in queryset.items():
                 uri += key + '=' + value[0] + '&'
             uri = quote(uri[:-1], safe='=&')
-            return "{0}?{1}".format(path, uri), queryset.get('nonce', ''),\
-                   queryset.get('code_challenge', ''), queryset.get('code_challenge_method', '')
+            return "{0}?{1}".format(path, uri), queryset.get('nonce', [''])[0],\
+                   queryset.get('code_challenge', [''])[0], queryset.get('code_challenge_method', [''])[0]
 
         if option == 'credentials' and 'openid' in kwargs['scopes']:
             kwargs['credentials'].update(nonce=kwargs['nonce'])
