07635e5c8284a0751678f1505c46090560885bde
97390483@qq.com <97390483@qq.com>
2020-08-31 14:11:05 +0800

    fix(executer.rdb): from loop create to bulk create for group_perm

--- executer/RDB/__init__.py
+++ executer/RDB/__init__.py
@@ -1,6 +1,9 @@
 '''
 RDB数据操作
 '''
+# pylint: disable=import-error
+# pylint: disable=no-self-use
+
 from rest_framework.exceptions import ValidationError
 from django.conf import settings
 from executer.core import Executer
@@ -31,7 +34,6 @@ class RDBExecuter(Executer):    # pylint: disable=abstract-method
     '''
     RDB数据操作接口
     '''
-
     def create_user(self, user_info):
         '''
         创建用户
@@ -61,6 +63,9 @@ class RDBExecuter(Executer):    # pylint: disable=abstract-method
         user.save()
 
     def delete_users(self, users):
+        """
+        delete redundant users
+        """
         for user in users:
             for dept_member in DeptMember.objects.filter(user=user):
                 dept_member.kill()
@@ -176,9 +181,12 @@ class RDBExecuter(Executer):    # pylint: disable=abstract-method
 
         group.order_no = Group.get_max_order_no(parent=group.parent) + 1
         group.save()
+        # 批量创建组权限
+        perm_ids = Perm.valid_objects.values_list('pk', flat=True)
+        group_perm_ids = GroupPerm.valid_objects.filter(owner=serializer.instance).values_list('perm_id', flat=True)
+        group_perms = [GroupPerm(owner=serializer.instance, perm_id=x) for x in perm_ids if x not in group_perm_ids]
+        GroupPerm.objects.bulk_create(group_perms)
 
-        for perm in Perm.valid_objects.all():
-            GroupPerm.valid_objects.get_or_create(owner=serializer.instance, perm=perm)
         return serializer.instance
 
     def add_group_to_group(self, group, parent_group):
