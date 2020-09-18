a4ecef942629673d484c314e691ae29dd52914b0
赵政然 <zhaozhengran@zhaozhengrandeMacBook-Pro.local>
2020-09-18 17:29:19 +0800

    fix(wechat qr): http method from POST to GET

--- siteapi/v1/tests/test_wechat.py
+++ siteapi/v1/tests/test_wechat.py
@@ -45,33 +45,31 @@ class UCenterTestCase(TestCase):
         wechat_user.save()
 
         client = self.client
-        res = client.post(reverse('siteapi:wechat_qr_callback'),\
-            data={'code':'test_auth_code'})
+        res = client.get(reverse('siteapi:wechat_qr_callback') + '?code=test_auth_code')
 
         self.assertEqual(res.status_code, 200)
         self.assertIn('token', res.json())
 
     @mock.patch('thirdparty_data_sdk.wechat_sdk.wechat_user_info_manager.WechatUserInfoManager.get_union_id')
-    def test_wechat_qr_login_newuser(self, mock_get_union_id):    # pylint: disable=invalid-name
+    def test_wechat_qr_login_newuser(self, mock_get_union_id):  # pylint: disable=invalid-name
         wechat_config = WechatConfig.get_current()
-        wechat_config.__dict__.update(app_id='test_app_id', app_private_key='test_app_private_key',\
-            wechat_public_key='test_wechat_public_key', qr_app_valid=True)
+        wechat_config.__dict__.update(app_id='test_app_id', app_private_key='test_app_private_key', \
+                                      wechat_public_key='test_wechat_public_key', qr_app_valid=True)
         wechat_config.save()
         mock_get_union_id.return_value = ''
         client = self.client
-        res = client.post(reverse('siteapi:wechat_qr_callback'),\
-            data={'code':'test_auth_code', 'app_id':'test_app_id'})
+        res = client.get(reverse('siteapi:wechat_qr_callback') + '?code=test_auth_code')
         expect = {'token': '', 'third_party_id': ''}
         self.assertEqual(res.json(), expect)
 
-    def test_wechat_qr_login_forbidden(self):    # pylint: disable=invalid-name
+    def test_wechat_qr_login_forbidden(self):  # pylint: disable=invalid-name
         client = self.client
         wechat_config = WechatConfig.get_current()
-        wechat_config.__dict__.update(app_id='app_id', app_private_key='app_private_key',\
-            wechat_public_key='wechat_public_key', qr_app_valid=False)
+        wechat_config.__dict__.update(app_id='app_id', app_private_key='app_private_key', \
+                                      wechat_public_key='wechat_public_key', qr_app_valid=False)
         wechat_config.save()
-        res = client.post(reverse('siteapi:wechat_qr_callback'),\
-            data={'code':'test_auth_code'})
+        res = client.get(reverse('siteapi:wechat_qr_callback'), \
+                          data={'code': 'test_auth_code'})
         expect_json = {'err_msg': 'wechat qr not allowed'}
         expect_code = 403
         self.assertEqual(res.json(), expect_json)
@@ -85,8 +83,8 @@ class UCenterTestCase(TestCase):
         user.save()
         mock_check_sms_token.side_effect = [{'mobile': '18812341234'}]
         mock_clear_sms_token.return_value = None
-        res = client.post(reverse('siteapi:wechat_bind'), data={'sms_token':\
-            'test_sms_token', 'user_id':'test_wechat_user_id'})
+        res = client.post(reverse('siteapi:wechat_bind'), data={'sms_token': \
+                                                                    'test_sms_token', 'user_id': 'test_wechat_user_id'})
         self.assertEqual(res.status_code, 201)
         self.assertIn('token', res.json())
 
