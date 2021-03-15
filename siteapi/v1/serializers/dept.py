b45fcc8db8b4d9ca4125eab7019fccd9cf7ad826
notevery <leaderjs@live.com>
2021-03-15 14:12:47 +0800

    单元测试修改

--- siteapi/v1/serializers/dept.py
+++ siteapi/v1/serializers/dept.py
@@ -254,6 +254,7 @@ class DeptTreeSerializer(DynamicFieldsModelSerializer, NodeSerialzierMixin):
             'node_subject': instance.node_subject,
             'uid': instance.uid,
             'name': instance.name,
+            'remark': instance.remark,
         }
 
     def get_users(self, instance):
