74537da586ccb41021736aceea8d4675f8919a54
inji-hanbin <hanbin@jinji-inc.com>
2021-04-07 15:00:17 +0800

    feat: 🎸 支持华为云短信
    支持华为的云短信发送
    
    ✅ Closes: #277

---
+++ extension_root/huawei_sms/__init__.py
@@ -0,0 +1,48 @@
+
+from runtime import Runtime
+from common.extension import InMemExtension
+from .constants import KEY
+from .serializers import HuaWeiSMSSerializer
+from .provider import HuaWeiSMSProvider
+
+
+class HuaWeiExtension(InMemExtension):
+
+    def start(self, runtime: Runtime, *args, **kwargs):
+
+        from extension.models import Extension
+        o = Extension.active_objects.filter(
+            type=KEY,
+        ).first()
+
+        assert o is not None
+        access_key = o.data.get('access_key')
+        secret_key = o.data.get('secret_key')
+        template = o.data.get('template')
+        signature = o.data.get('signature')
+        sender = o.data.get('sender')
+
+        sms_provider = HuaWeiSMSProvider(
+            access_key=access_key,
+            secret_key=secret_key,
+            template=template,
+            signature=signature,
+            sender=sender,
+        )
+
+        print('>>>>', sms_provider)
+
+        runtime.sms_provider = sms_provider
+
+        super().start(runtime=runtime, *args, **kwargs)
+
+
+extension = HuaWeiExtension(
+    name=KEY,
+    description="""基于华为云平台的短信发送功能""",
+    version='1.0',
+    homepage='https://www.longguikeji.com',
+    logo='',
+    maintainer='insfocus@gmail.com',
+    serializer=HuaWeiSMSSerializer,
+)
