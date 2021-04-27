a7d6e56a871d0cb27005089dff596ff54d7f5f60
inji-hanbin <hanbin@jinji-inc.com>
2021-04-27 14:32:21 +0800

    feat: 🎸 保存        if algorithm and app.type == 'OIDC':
    保存algorithm

--- extension_root/oauth2_authorization_server/provider.py
+++ extension_root/oauth2_authorization_server/provider.py
@@ -15,10 +15,13 @@ class OAuth2AppTypeProvider(AppTypeProvider):
         client_type = data.get('client_type')
         redirect_uris = data.get('redirect_uris')
         authorization_grant_type = data.get('grant_type')
+        algorithm = data.get('algorithm')
         obj = Application()
         obj.name = app.id
         obj.client_type = client_type
         obj.redirect_uris = redirect_uris
+        if algorithm and app.type == 'OIDC':
+            obj.algorithm = algorithm
         obj.authorization_grant_type = authorization_grant_type
         obj.save()
 
@@ -32,17 +35,21 @@ class OAuth2AppTypeProvider(AppTypeProvider):
             'authorize': reverse("api:oauth2_authorization_server:authorize", args=[app.tenant.uuid]),
             'token': reverse("api:oauth2_authorization_server:token", args=[app.tenant.uuid]),
         }
-
+        if algorithm and app.type == 'OIDC':
+            uniformed_data['algorithm'] = obj.algorithm
         return uniformed_data
 
     def update(self, app, data: Dict) -> Dict:
         client_type = data.get('client_type')
         redirect_uris = data.get('redirect_uris')
         authorization_grant_type = data.get('grant_type')
+        algorithm = data.get('algorithm')
         obj = Application.objects.filter(name=app.id).first()
         obj.client_type = client_type
         obj.redirect_uris = redirect_uris
         obj.authorization_grant_type = authorization_grant_type
+        if algorithm and app.type == 'OIDC':
+            obj.algorithm = algorithm
         obj.save()
         uniformed_data = {
             'client_type': client_type,
@@ -54,4 +61,6 @@ class OAuth2AppTypeProvider(AppTypeProvider):
             'authorize': reverse("api:oauth2_authorization_server:authorize", args=[app.tenant.uuid]),
             'token': reverse("api:oauth2_authorization_server:token", args=[app.tenant.uuid]),
         }
+        if algorithm and app.type == 'OIDC':
+            uniformed_data['algorithm'] = obj.algorithm
         return uniformed_data
