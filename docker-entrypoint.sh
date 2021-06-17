72c115827a3f25c0ae90aa6c4c437cdd2a166a9b
notevery <leaderjs@live.com>
2021-06-17 17:53:10 +0800

    feat: 容器去掉修改owner为arker，无法修改configmap挂载的配置文件

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@ set -ex
 if [ "$1" = 'tini' -a "$(id -u)" = '0' ]; then
     /usr/local/bin/python3.8 manage.py compilemessages;
     /usr/local/bin/python3.8 manage.py migrate;
-    chown -R arker:arker /var/arkid;
+    # 无法修改configmap挂载的配置文件
+    # chown -R arker:arker /var/arkid;
     exec gosu arker "$0" "$@";
 fi
 
