e4701879fca0e134051a97bf6afbb1f364f77b90
notevery <leaderjs@live.com>
2021-06-18 18:04:53 +0800

    feat: 修改容器启动用户为arkid

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ if [ "$1" = 'tini' -a "$(id -u)" = '0' ]; then
     /usr/local/bin/python3.8 manage.py compilemessages;
     /usr/local/bin/python3.8 manage.py migrate;
     # 无法修改configmap挂载的配置文件
-    # chown -R arker:arker /var/arkid;
-    exec gosu arker "$0" "$@";
+    # chown -R arkid:arkid /var/arkid;
+    exec gosu arkid "$0" "$@";
 fi
 
 exec "$@"
