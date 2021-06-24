ec498a55c72bb902c22abe8839c8009da1980964
fanhe <fanhe@longguikeji.com>
2021-06-25 00:32:00 +0800

    feat: 注册成功后跳转到完善用户资料页面可配置，支持用除了username之外的原生字段登录

--- api/v1/serializers/tenant.py
+++ api/v1/serializers/tenant.py
@@ -89,6 +89,18 @@ class ConfigSerializer(serializers.Serializer):
     register_time_limit = serializers.IntegerField(label=_('用户注册时间限制(分钟)'))
     register_count_limit = serializers.IntegerField(label=_('用户注册数量限制'))
     serializers.ListField(child=serializers.CharField(), label=_('允许上传的文件格式'))
+    username_login_enabled = serializers.BooleanField(label=_('是否打开用户名登录'))
+    username_register_enabled = serializers.BooleanField(label=_('是否打开用户名注册'))
+    mobile_login_enabled = serializers.BooleanField(label=_('是否打开手机号登录'))
+    mobile_register_enabled = serializers.BooleanField(label=_('是否打开手机号注册'))
+    need_complete_profile_after_register = serializers.BooleanField(label=_('注册完成后是否跳转到完善用户资料页面'))
+    can_skip_complete_profile = serializers.BooleanField(label=_('完善用户资料页面是否可以跳过'))
+    native_login_field_name = serializers.CharField(label=_('用于登录的原生字段名'))
+    native_login_enabled = serializers.BooleanField(label=_('开启用原生字段登录'))
+    native_register_enabled = serializers.BooleanField(label=_('开启用原生字段注册'))
+    custom_login_field_uuid = serializers.CharField(label=_('用于登录的自定义字段UUID'))
+    custom_login_enabled = serializers.BooleanField(label=_('开启用自定义字段登录'))
+
 
 
 class TenantConfigSerializer(BaseDynamicFieldModelSerializer):
