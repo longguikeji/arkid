6f0a01bd7530937e772e24743dc13fa608443a8f
longgui-penglei <penglei@longguikeji.com>
2021-06-28 15:20:58 +0800

    feat: 🎸 使用reset_password接口, 在user中添加一些页面生成信息

--- api/v1/pages/user.py
+++ api/v1/pages/user.py
@@ -29,8 +29,14 @@ extend_schema_tags(
         },
         'item': {
             'password': {
-                'path': '/api/v1/user/reset_password/',
-                'method': 'post'
+                'read': {
+                    'path': '/api/v1/tenant/{parent_lookup_tenant}/user/{id}/',
+                    'method': 'get' 
+                },
+                'write': {
+                    'path': '/api/v1/user/reset_password/',
+                    'method': 'post'
+                }
             },
             'update': {
                 'read': {
