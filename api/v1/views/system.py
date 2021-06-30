89c3a626ea14716c7f559a0bec27e45e3ea87a44
inji-hanbin <hanbin@jinji-inc.com>
2021-06-30 15:30:10 +0800

    feat: 🎸 增强权限验证的部分

--- api/v1/views/system.py
+++ api/v1/views/system.py
@@ -6,6 +6,7 @@ from api.v1.serializers.system import SystemConfigSerializer
 from rest_framework_expiring_authtoken.authentication import ExpiringTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from system.models import SystemConfig
+from system.permission import IsSuperAdmin
 from runtime import get_app_runtime
 from common.code import Code
 
@@ -13,7 +14,7 @@ from common.code import Code
 @extend_schema(roles=['global admin'], tags=['system'])
 class SystemConfigView(generics.RetrieveUpdateAPIView):
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsSuperAdmin]
     authentication_classes = [ExpiringTokenAuthentication]
 
     serializer_class = SystemConfigSerializer
@@ -33,12 +34,3 @@ class SystemConfigView(generics.RetrieveUpdateAPIView):
                 data['is_open_register'] = True
             systemconfig.save()
         return systemconfig
-
-    @extend_schema(roles=['global admin'])
-    def update(self, request, *args, **kwargs):
-        if not request.user.is_superuser:
-            return JsonResponse(data={
-                'error': Code.PERMISSION_CHECK_ERROR.value,
-                'message': _('You are not an administrator.'),
-            })
-        return super().update(request, *args, **kwargs)
