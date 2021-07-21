e50cd00b377293429fde6d2de8ed04fc1cce747c
fanhe <fanhe@longguikeji.com>
2021-07-21 14:44:39 +0800

    feat: 系统配置改成按照不同分类保存在dict中

--- api/v1/urls/system.py
+++ api/v1/urls/system.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from api.v1.views import (
-    system as views_system
-)
+from api.v1.views import system as views_system
 
 urlpatterns = [
-    path('system/config', views_system.SystemConfigView.as_view(), name='system-config'),
+    re_path(
+        r'^system/config/(?P<subject>[\w-]+)/$',
+        views_system.SystemConfigView.as_view(),
+        name='system-config-subject',
+    ),
 ]
--- api/v1/views/system.py
+++ api/v1/views/system.py
@@ -2,16 +2,37 @@ from django.http.response import JsonResponse
 from rest_framework import generics
 from django.utils.translation import gettext_lazy as _
 from openapi.utils import extend_schema
-from api.v1.serializers.system import SystemConfigSerializer
+from api.v1.serializers.system import (
+    SystemConfigSerializer,
+    LoginRegisterConfigSerializer,
+    PrivacyNoticeConfigSerializer,
+)
 from rest_framework_expiring_authtoken.authentication import ExpiringTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound, ValidationError
+from rest_framework.response import Response
 from system.models import SystemConfig
 from system.permission import IsSuperAdmin
 from runtime import get_app_runtime
 from common.code import Code
+from drf_spectacular.utils import PolymorphicProxySerializer
+
+SystemConfigPolymorphicProxySerializer = PolymorphicProxySerializer(
+    component_name='SystemConfigPolymorphicProxySerializer',
+    serializers={
+        'login_register': LoginRegisterConfigSerializer,
+        'privacy_notice': PrivacyNoticeConfigSerializer,
+    },
+    resource_type_field_name='subject',
+)
 
 
-@extend_schema(roles=['global admin'], tags=['system'])
+@extend_schema(
+    roles=['global admin'],
+    tags=['system'],
+    request=SystemConfigPolymorphicProxySerializer,
+    responses=SystemConfigPolymorphicProxySerializer,
+)
 class SystemConfigView(generics.RetrieveUpdateAPIView):
 
     permission_classes = [IsAuthenticated, IsSuperAdmin]
@@ -19,18 +40,48 @@ class SystemConfigView(generics.RetrieveUpdateAPIView):
 
     serializer_class = SystemConfigSerializer
 
+    def get_serializer_class(self):
+        if 'subject' not in self.kwargs:
+            return SystemConfigSerializer
+
+        subject = self.kwargs.get('subject')
+
+        if subject == 'login_register':
+            return LoginRegisterConfigSerializer
+
+        if subject == 'privacy_notice':
+            return PrivacyNoticeConfigSerializer
+
+        raise NotFound
+
     def get_object(self):
         systemconfig, is_created = SystemConfig.objects.get_or_create(
             is_del=False,
         )
         if is_created is True:
-            systemconfig.data = {
-                'is_open_register': True
-            }
-            systemconfig.save()
-        else:
-            data = systemconfig.data
-            if 'is_open_register' not in data:
-                data['is_open_register'] = True
-            systemconfig.save()
+            # 初始化配置
+            pass
         return systemconfig
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = SystemConfigSerializer(instance)
+        data = serializer.data
+        subject = self.kwargs.get('subject', '')
+        if subject:
+            data = data.get('data')
+            return Response({subject: data.get(subject)})
+        else:
+            return Response(data.get('data'))
+
+    def update(self, request, *args, **kwargs):
+        subject = self.kwargs.get('subject', '')
+        instance = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(instance=instance, subject=subject)
+        data = instance.data
+        if subject:
+            return Response(data.get(subject))
+        else:
+            return Response(data)
