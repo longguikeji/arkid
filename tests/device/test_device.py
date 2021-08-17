4cd59ebb71bb3544319776d1a11ebc75ac49c2d6
inji-hanbin <hanbin@jinji-inc.com>
2021-08-17 17:55:54 +0800

    feat: 🎸 完善device的test

--- tests/device/test_device.py
+++ tests/device/test_device.py
@@ -4,7 +4,8 @@ test device
 from unittest import mock
 from django.test import TestCase
 
-from tenant.models import Tenant, TenantDevice
+from tenant.models import Tenant
+from device.models import Device
 from inventory.models import User
 from django.contrib.auth.hashers import make_password
 from django.test import Client, RequestFactory, TestCase
@@ -30,50 +31,48 @@ class DeviceTestCase(TestCase):
         )
         self.token = self.user.token
         # create device
-        # self.device = TenantDevice.objects.create(
-        #     tenant=self.tenant,
-        #     device_type='pc',
-        #     system_version='mac 10.11',
-        #     browser_version='chrome 3.0.11',
-        #     ip='127.0.0.1',
-        #     mac_address='12:13:14:15',
-        #     device_number='sdasdas',
-        #     device_id='12345678',
-        #     account_ids=['123456','7890']
-        # )
+        self.device = Device.objects.create(
+            device_type='pc',
+            system_version='mac 10.11',
+            browser_version='chrome 3.0.11',
+            ip='127.0.0.1',
+            mac_address='12:13:14:15',
+            device_number='sdasdas',
+            device_id='12345678',
+            account_ids=['123456']
+        )
         self.client = Client(HTTP_AUTHORIZATION='Token {}'.format(self.token))
 
-    # def test_device_create(self):
-    #     url = '/api/v1/tenant/{}/device/'.format(self.tenant.uuid)
-    #     body = {
-    #         "device_type": "手机",
-    #         "system_version": "mac 10.11",
-    #         "browser_version": "chrome 3.0",
-    #         "ip": "127.0.0.1",
-    #         "mac_address": "12:12:33:44:22",
-    #         "device_number": "83621321",
-    #         "device_id": "123",
-    #         "account_ids": ["12321321", "12321321"]
-    #     }
-    #     resp = self.client.post(url, body, content_type='application/json')
-    #     self.assertEqual(resp.status_code, 201, resp.content.decode())
-    #     result = json.loads(resp.content.decode())
-    #     self.assertIsNotNone(result.get('uuid'))
+    def test_device_create(self):
+        url = '/api/v1/device/'
+        body = {
+            "device_type": "手机",
+            "system_version": "mac 10.11",
+            "browser_version": "chrome 3.0",
+            "mac_address": "12:12:33:44:22",
+            "device_number": "83621321",
+            "device_id": "123",
+            "account_ids": ["12321321"]
+        }
+        resp = self.client.post(url, body, content_type='application/json')
+        self.assertEqual(resp.status_code, 201, resp.content.decode())
+        result = json.loads(resp.content.decode())
+        self.assertIsNotNone(result.get('uuid'))
     
-    # def test_device_list(self):
-    #     url = '/api/v1/tenant/{}/device/'.format(self.tenant.uuid)
-    #     resp = self.client.get(url, content_type='application/json')
-    #     self.assertEqual(resp.status_code, 200, resp.content.decode())
-    #     result = json.loads(resp.content.decode())
-    #     count = result.get('count')
-    #     self.assertIsNotNone(count)
+    def test_device_list(self):
+        url = '/api/v1/device/'
+        resp = self.client.get(url, content_type='application/json')
+        self.assertEqual(resp.status_code, 200, resp.content.decode())
+        result = json.loads(resp.content.decode())
+        count = result.get('count')
+        self.assertIsNotNone(count)
     
-    # def test_device_export(self):
-    #     url = '/api/v1/tenant/{}/device_export/'.format(self.tenant.uuid)
-    #     resp = self.client.get(url, content_type='application/json')
-    #     self.assertEqual(resp.status_code, 200, resp.content.decode())
+    def test_device_export(self):
+        url = '/api/v1/device_export/'
+        resp = self.client.get(url, content_type='application/json')
+        self.assertEqual(resp.status_code, 200, resp.content.decode())
 
-    # def test_device_delete(self):
-    #     url = '/api/v1/tenant/{}/device/{}/detail/'.format(self.tenant.uuid, self.device.uuid)
-    #     resp = self.client.delete(url, content_type='application/json')
-    #     self.assertEqual(resp.status_code, 204, resp.content.decode())
+    def test_device_delete(self):
+        url = '/api/v1/device/{}/detail/'.format(self.device.uuid)
+        resp = self.client.delete(url, content_type='application/json')
+        self.assertEqual(resp.status_code, 204, resp.content.decode())
