30e5883140f4c53993dd85d1d89e9657b5b4e62a
longgui-penglei <penglei@longguikeji.com>
2021-08-25 11:20:16 +0800

    feat: 🎸 更新系统配置页面的tag信息

--- api/v1/pages/system_config.py
+++ api/v1/pages/system_config.py
@@ -1,8 +1,79 @@
 from openapi.utils import extend_schema_tags
 
-tag = [ 'system_config', 'system_register_privacy_notice' ]
+tag = [ 'system_login_register_extension', 'system_config', 'system_register_privacy_notice' ]
 path = 'system_config'
 name = '系统配置'
+
+system_login_register_extension_tag = 'system_login_register_extension'
+system_login_register_extension_name = '系统平台登录注册插件化配置'
+
+extend_schema_tags(
+    system_login_register_extension_tag,
+    system_login_register_extension_name,
+    {
+        'type': 'table_page',
+        'init': {
+            'path': '/api/v1/login_register_config/',
+            'method': 'get'
+        },
+        'global': {
+            'create': {
+                'tag': 'system_login_register_extension.create'
+            }
+        },
+        'local': {
+            'update': {
+                'tag': 'system_login_register_extension.update'
+            },
+            'delete': {
+                'path': '/api/v1/login_register_config/{id}/',
+                'method': 'delete'
+            }
+        }
+    }
+)
+
+system_login_register_extension_create_tag = 'system_login_register_extension.create'
+system_login_register_extension_create_name = '创建系统平台登录注册插件'
+
+extend_schema_tags(
+    system_login_register_extension_create_tag,
+    system_login_register_extension_create_name,
+    {
+        'type': 'form_page',
+        'init': {
+            'path': '/api/v1/login_register_config/',
+            'method': 'post'
+        },
+        'global': {
+            'create': {
+                'path': '/api/v1/login_register_config/',
+                'method': 'post'
+            }
+        }
+    }
+)
+
+system_login_register_extension_update_tag = 'system_login_register_extension.update'
+system_login_register_extension_update_name = '编辑系统平台登录注册插件'
+
+extend_schema_tags(
+    system_login_register_extension_update_tag,
+    system_login_register_extension_update_name,
+    {
+        'type': 'form_page',
+        'init': {
+            'path': '/api/v1/login_register_config/{id}/',
+            'method': 'get',
+        },
+        'global': {
+            'update': {
+                'path': '/api/v1/login_register_config/{id}/',
+                'method': 'put'
+            }
+        }
+    }
+)
 
 system_config_tag = 'system_config'
 system_config_name = '系统配置'
