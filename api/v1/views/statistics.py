d8dc24ee08bab3eea397b3466c48f1d1fcf28450
luolu-lg <louis.law@hotmail.com>
2021-09-24 07:51:33 +0000

    fix: 🐛 添加更多图表

--- api/v1/views/statistics.py
+++ api/v1/views/statistics.py
@@ -1,4 +1,5 @@
 import copy
+import json
 import datetime
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -10,6 +11,7 @@ from rest_framework_expiring_authtoken.authentication import ExpiringTokenAuthen
 
 from log.models import Log
 from tenant.models import Tenant
+from inventory.models import User
 
 
 @extend_schema(
@@ -29,10 +31,12 @@ class StatisticsView(BaseTenantViewSet, viewsets.ReadOnlyModelViewSet):
         'yAxis': {
             'type': 'value'
         },
+        'legend': {},
         'series': [
             {
                 'data': [150, 230, 224, 218, 135, 147, 260],
-                'type': 'line'
+                'type': 'line',
+                'name': '',
             }
         ],
         "title": {
@@ -64,6 +68,7 @@ class StatisticsView(BaseTenantViewSet, viewsets.ReadOnlyModelViewSet):
 
         chart['xAxis']['data'] = xAxis
         chart['series'][0]['data'] = data
+        chart['series'][0]['name'] = '30天'
         chart['title'] = {
             "text": "用户注册人数变化图",
             "subtext": "过去30天"
@@ -75,7 +80,7 @@ class StatisticsView(BaseTenantViewSet, viewsets.ReadOnlyModelViewSet):
         sql = """
             SELECT COUNT(*) as id, cast(created as date) as date
             from log_log 
-            where data->"$.request.full_path" like "_/api/v1/%%/login/_" 
+            where data->"$.request.full_path" = "/api/v1/login/"
             and data->"$.response.status_code"=200
             and DATEDIFF(NOW(), created)<30
             GROUP BY cast(created as date) order by date desc;
@@ -94,6 +99,7 @@ class StatisticsView(BaseTenantViewSet, viewsets.ReadOnlyModelViewSet):
 
         chart['xAxis']['data'] = xAxis
         chart['series'][0]['data'] = data
+        chart['series'][0]['name'] = '30天'
         chart['title'] = {
             "text": "用户登录人数变化图",
             "subtext": "过去30天"
@@ -101,8 +107,74 @@ class StatisticsView(BaseTenantViewSet, viewsets.ReadOnlyModelViewSet):
 
         return chart
 
+    def gen_register_user_list(self):
+        sql = """
+            select distinct id from(
+                SELECT data->"$.response.data" as id
+                from log_log
+                where data->"$.request.full_path" like "_/api/v1/tenant/%%/user/_"
+                and data->"$.response.status_code"=201
+                order by created desc) as b
+            limit 10;
+            """
+        query = Log.objects.raw(sql)
+        res = []
+        for r in query:
+            try:
+                d = json.loads(json.loads(r.id))
+                username = d["username"]
+                if username not in res:
+                    res.append(username)
+            except:
+                pass
+
+        chart = {
+            'data': res,
+            'title': {
+                "text": "最近注册人列表",
+                "subtext": "最近10个"
+            },
+            'type': 'list',
+        }
+        return chart
+
+    def gen_login_user_list(self):
+        sql = """
+            select distinct id from(
+                SELECT data->"$.request.data" as id
+                from log_log
+                where data->"$.request.full_path" = "/api/v1/login/"
+                and data->"$.response.status_code"=200
+                order by created desc) as b
+            limit 10;
+            """
+        query = Log.objects.raw(sql)
+        res = []
+        for r in query:
+            try:
+                d = json.loads(json.loads(r.id))
+                user_uuid = d["data"]["user_uuid"]
+                user = User.objects.filter(uuid=user_uuid).first()
+                username = user.username
+                if username not in res:
+                    res.append(username)
+            except:
+                pass
+
+        chart = {
+            'data': res,
+            'title': {
+                "text": "最近登录人列表",
+                "subtext": "最近10个"
+            },
+            'type': 'list',
+        }
+        return chart
+
     def list(self, request, parent_lookup_tenant):
         charts = []
         charts.append(self.gen_register_user_chart())
         charts.append(self.gen_login_user_chart())
+        charts.append(self.gen_register_user_list())
+        charts.append(self.gen_login_user_list())
         return Response(charts)
