dd2fe6629db9faeff1c5fbc01d6ea535b1d45aef
luolu-lg <louis.law@hotmail.com>
2021-10-09 00:51:19 +0000

    fix: 🐛 url parameter

--- api/v1/pages/statistics.py
+++ api/v1/pages/statistics.py
@@ -10,7 +10,7 @@ extend_schema_tags(
     {
         'type': 'dashboard_page',
         'init': {
-            'path': '/api/v1/tenant/{parent_lookup_tenant}/statistics/',
+            'path': '/api/v1/tenant/{tenant_uuid}/statistics/',
             'method': 'get'
         }
     }
