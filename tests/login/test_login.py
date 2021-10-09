13137b8a6c6933bcf95bbfbbef4c3f51885f7c7f
fanhe <fanhe@longguikeji.com>
2021-10-09 21:23:15 +0800

    fix: 修改登录测试用例

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -13,11 +13,16 @@ from app.models import App
 from common.code import Code
 from runtime import get_app_runtime
 from api.v1.serializers.sms import LoginSMSClaimSerializer
+from login_register_config.models import LoginRegisterConfig
 
 import json
 
-from extension_root.redis_cache import extension
+from extension_root.redis_cache import extension as redis_extension
+from extension_root.mobile_login_register import extension as mobile_extension
 from django.conf import settings
+
+DEFAULT_LOGIN_REGISTER_EXTENSION = 'password_login_register'
+MOBILE_LOGIN_REGISTER_EXTENSION = 'mobile_login_register'
 
 
 class LoginTestCase(TestCase):
@@ -35,20 +40,29 @@ class LoginTestCase(TestCase):
             username='superuser',
             password=make_password('password1'),
         )
+        self.user.tenants.add(self.tenant)
         self.token = self.user.token
         self.client = Client(HTTP_AUTHORIZATION='Token {}'.format(self.token))
         self.runtime = get_app_runtime()
-        extension.start(self.runtime)
+        redis_extension.start(self.runtime)
+        mobile_extension.start(self.runtime)
 
     def test_username_login(self):
         tenant_uuid = self.tenant.uuid
-        field_names = 'username'
-        field_uuids = ''
         url = (
-            reverse("api:tenant-secret-login", args=[tenant_uuid])
-            + f'?field_names={field_names}&field_uuids={field_uuids}'
+            reverse("api:login")
+            + f'?tenant={tenant_uuid}'
         )
-        body = {'username': 'superuser', 'password': 'password1'}
+        config_data = {
+            'login_enabled': True,
+            'register_enabled': True,
+            'login_enabled_field_names': ['username'],
+            'register_enabled_field_names': ['username'],
+        }
+        config = LoginRegisterConfig.valid_objects.create(
+            tenant=self.tenant, type=DEFAULT_LOGIN_REGISTER_EXTENSION, data=config_data
+        )
+        body = {'username': 'superuser', 'password': 'password1', 'config_uuid': config.uuid.hex}
         resp = self.client.post(url, body, content_type='application/json')
         self.assertEqual(resp.status_code, 200, resp.content.decode())
         result = json.loads(resp.content.decode())
@@ -65,13 +79,18 @@ class LoginTestCase(TestCase):
         key = LoginSMSClaimSerializer.gen_sms_code_key(mobile)
         value = '222333'
         self.runtime.cache_provider.set(key, value, settings.SMS_LIFESPAN.seconds)
-        url = reverse(
-            "api:tenant-mobile-login",
-            args=[
-                tenant_uuid,
-            ],
+        url = (
+            reverse("api:login")
+            + f'?tenant={tenant_uuid}'
         )
-        body = {'mobile': mobile, 'code': value}
+        config_data = {
+            'login_enabled': True,
+            'register_enabled': True,
+        }
+        config = LoginRegisterConfig.valid_objects.create(
+            tenant=self.tenant, type=MOBILE_LOGIN_REGISTER_EXTENSION, data=config_data
+        )
+        body = {'mobile': mobile, 'code': value, 'config_uuid': config.uuid.hex}
         resp = self.client.post(url, body, content_type='application/json')
         self.assertEqual(resp.status_code, 200, resp.content.decode())
         result = json.loads(resp.content.decode())
