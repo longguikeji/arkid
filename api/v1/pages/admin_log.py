eab69efdc839899f78bf21deb66f90b4d79a9493
longgui-penglei <penglei@longguikeji.com>
2021-11-22 22:51:52 +0800

    feat: 🎸 update tag

--- api/v1/pages/admin_log.py
+++ api/v1/pages/admin_log.py
@@ -12,6 +12,28 @@ extend_schema_tags(
         'init': {
             'path': '/api/v1/tenant/{parent_lookup_tenant}/admin_log/',
             'method': 'get'
+        },
+        'local': {
+            'detail': {
+                'tag': 'admin_log.detail',
+                'icon': 'el-icon-reading',
+                'description': '查看详情'
+            }
+        }
+    }
+)
+
+admin_log_detail_tag = 'admin_log.detail'
+admin_log_detail_name = '用户行为日志详情'
+
+extend_schema_tags(
+    admin_log_detail_tag,
+    admin_log_detail_name,
+    {
+        'type': 'form_page',
+        'init': {
+            'path': '/api/v1/tenant/{parent_lookup_tenant}/admin_log/{id}/',
+            'method': 'get'
         }
     }
 )
--- api/v1/pages/user_log.py
+++ api/v1/pages/user_log.py
@@ -12,6 +12,28 @@ extend_schema_tags(
         'init': {
             'path': '/api/v1/tenant/{parent_lookup_tenant}/user_log/',
             'method': 'get'
+        },
+        'local': {
+            'detail': {
+                'tag': 'user_log.detail',
+                'icon': 'el-icon-reading',
+                'description': '查看详情'
+            }
+        }
+    }
+)
+
+user_log_detail_tag = 'user_log.detail'
+user_log_detail_name = '用户行为日志详情'
+
+extend_schema_tags(
+    user_log_detail_tag,
+    user_log_detail_name,
+    {
+        'type': 'form_page',
+        'init': {
+            'path': '/api/v1/tenant/{parent_lookup_tenant}/user_log/{id}/',
+            'method': 'get'
         }
     }
 )
