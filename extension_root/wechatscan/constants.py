933ebdf1859d50645e1bca25a817f80471d3b176
inji-hanbin <hanbin@jinji-inc.com>
2022-01-13 19:31:28 +0800

    feat: 🎸 能够微信扫码登录

--- extension_root/wechatscan/constants.py
+++ extension_root/wechatscan/constants.py
@@ -3,8 +3,9 @@ GET_TOKEN_URL = 'https://api.weixin.qq.com/sns/oauth2/access_token'
 GET_USERINFO_URL = 'https://api.weixin.qq.com/sns/userinfo'
 FRESH_TOKEN_URL = 'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={}&grant_type=refresh_token&refresh_token={}'
 
+
 # errcode msg map
 CLIENT_VALID = 'bad_verification_code'
 
 KEY = 'wechatscan'
-IMG_URL = 'https://wechatscan.com/assets/favicon.ico'
+IMG_URL = 'https://src.onlinedown.net/d/file/p/2019-08-06/2dc4d1b597725835d85259829db3fcff.jpg'
