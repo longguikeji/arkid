06a92010675997929b651a9a164e1656b145c1c0
Luo Lu <louis.law@hotmail.com>
2022-03-10 07:44:51 +0000

    fix: 🐛 fix bind_saas to create oidc app

--- api/v1/views/bind_saas.py
+++ api/v1/views/bind_saas.py
@@ -13,6 +13,7 @@ from runtime import get_app_runtime
 from app.models import App
 from common.provider import AppTypeProvider
 from django.conf import settings
+from oauth2_provider.models import Application
 from api.v1.serializers.bind_saas import ArkIDBindSaasSerializer, ArkIDBindSaasCreateSerializer
 
 
@@ -32,11 +33,13 @@ class ArkIDBindSaasAPIView(GenericAPIView):
         本地租户绑定Saas租户
         """
         tenant = Tenant.objects.get(uuid=tenant_uuid)
+
+        bind_info = self.get_bind_info(tenant_uuid)
+        if bind_info.get('saas_tenant_slug'):
+            bind_info = self.update_saas_binding(tenant, request)
+            return Response(bind_info, HTTP_200_OK)
         
-        status, app = self.create_oidc_app(tenant)
-        if status == 'error':
-            data = {'error': '已绑定'}
-            return Response(data, HTTP_200_OK)
+        app = self.create_oidc_app()
 
         try:
             resp = self.create_saas_binding(tenant, request, app)
@@ -47,6 +50,9 @@ class ArkIDBindSaasAPIView(GenericAPIView):
             app.kill()
             data = {'error': str(e)}
             return Response(data, HTTP_200_OK)
+
+        app.redirect_uris = resp['callback_url']
+        app.save()
 
         data = {
             'saas_tenant_uuid': resp['saas_tenant_uuid'],
@@ -59,49 +65,39 @@ class ArkIDBindSaasAPIView(GenericAPIView):
         """
         查询 saas 绑定信息
         """
+        bind_info = self.get_bind_info(tenant_uuid)
+        return Response(bind_info, HTTP_200_OK)
+
+    @extend_schema(roles=['tenant admin', 'global admin'], request=ArkIDBindSaasSerializer)
+    def patch(self, request, tenant_uuid, *args, **kwarg):
+        """
+        查询 saas 绑定信息
+        """
+        tenant = Tenant.objects.get(uuid=tenant_uuid)
+        bind_info = self.update_saas_binding(tenant, request)
+        return Response(bind_info, HTTP_200_OK)
+
+    def get_bind_info(self, tenant_uuid):
         bind_saas_url = settings.ARKID_SAAS + '/api/v1/arkid/saas/bind'
         params = {'local_tenant_uuid': tenant_uuid}
         resp = requests.get(bind_saas_url, params=params).json()
-        return Response(resp, HTTP_200_OK)
+        return resp
 
-    def create_oidc_app(self, tenant):
-        url = settings.ARKID_SAAS
-        kwargs = {
-            'tenant': tenant,
-            'name': 'arkid_saas',
-            'type': 'OIDC',
-        }
-        defaults = {
-            'url': url,
-            'description': '连接中心平台OIDC应用',
-            'logo': '',
-        }
-        app, created = App.active_objects.update_or_create(**kwargs, defaults=defaults)
-        if not created:
-            return 'error', app
-
+    def create_oidc_app(self):
         redirect_uris = ''
-        oidc_data = {
+        defaults = {
             'client_type': 'public',
             'redirect_uris': redirect_uris,
-            'grant_type': 'authorization-code',
+            'authorization_grant_type': 'authorization-code',
             'skip_authorization': True,
             'algorithm': 'RS256',
         }
-        try:
-            r = get_app_runtime()
-            provider_cls: AppTypeProvider = r.app_type_providers.get('OIDC', None)
-            assert provider_cls is not None
-            provider = provider_cls()
-            data = provider.create(app=app, data=oidc_data)
-            if data is not None:
-                app.data = data
-                app.save()
-        except Exception as e:
-            app.kill()
-            raise Exception(e)
-        
-        return 'ok', app
+
+        app, created = Application.objects.update_or_create(
+            name = 'arkid_saas',
+            defaults=defaults,
+        )
+        return app
 
     def create_saas_binding(self, tenant, request, app):
         bind_saas_url = settings.ARKID_SAAS + '/api/v1/arkid/saas/bind'
@@ -113,29 +109,31 @@ class ArkIDBindSaasAPIView(GenericAPIView):
             'contact_person': request.data['contact_person'],
             'email': request.data['email'],
             'mobile': request.data['mobile'],
-            'client_id': app.data['client_id'],
-            'client_secret': app.data['client_secret'],
+            'client_id': app.client_id,
+            'client_secret': app.client_secret,
             'local_host': host,
             'saas_tenant_slug': request.data['saas_tenant_slug'],
         }
         resp = requests.post(bind_saas_url, json=params).json()
-
-        redirect_uris = resp['callback_url']
-        oidc_data = {
-            'client_type': 'public',
-            'redirect_uris': redirect_uris,
-            'grant_type': 'authorization-code',
-            'skip_authorization': True,
-            'algorithm': 'RS256',
-        }
-        r = get_app_runtime()
-        provider_cls: AppTypeProvider = r.app_type_providers.get('OIDC', None)
-        assert provider_cls is not None
-        provider = provider_cls()
-        data = provider.update(app=app, data=oidc_data)
-        if data is not None:
-            app.data = data
-        app.url = f"{settings.ARKSTOER_URL.rstrip('/')}/api/v1/login?tenant_slug={resp['saas_tenant_slug']}"
-        app.save()
-
         return resp
+
+    def update_saas_binding(self, tenant, request):
+        bind_saas_url = settings.ARKID_SAAS + '/api/v1/arkid/saas/bind'
+        host = get_app_config().get_host()
+        params = {
+            'local_tenant_uuid': str(tenant.uuid),
+            'company_name': request.data['company_name'],
+            'contact_person': request.data['contact_person'],
+            'email': request.data['email'],
+            'mobile': request.data['mobile'],
+        }
+        resp = requests.patch(bind_saas_url, json=params).json()
+        return resp
+
+    def create_arkidstore_login_app(self, tenant, saas_tenant_slug):
+        # url = f"{settings.ARKSTOER_URL.rstrip('/')}/api/v1/login?tenant_slug={resp['saas_tenant_slug']}"
+        pass
+
+    def create_arkid_saas_login_app(self, tenant, saas_tenant_slug):
+        # url = settings.ARKID_SAAS
+        pass
