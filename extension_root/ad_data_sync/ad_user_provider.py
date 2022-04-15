83d5f383f36bebc8a48580e1d6311125195212a2
fanhe <fanhe@longguikeji.com>
2022-04-15 15:36:03 +0800

    feat: 🎸 优化ad_data_sync server代码

--- extension_root/ad_data_sync/ad_user_provider.py
+++ extension_root/ad_data_sync/ad_user_provider.py
@@ -16,21 +16,32 @@ from scim_server.schemas.comparison_operator import ComparisonOperator
 from scim_server.schemas.attribute_names import AttributeNames
 from ldap3 import Server, Connection
 from data_sync.models import DataSyncConfig
-from .utils import get_ad_connection
+from .utils import get_ad_connection, get_scim_user
 from scim_server.schemas.core2_enterprise_user import Core2EnterpriseUser
+from common.logger import logger
 
 
 class AdUserProvider(ProviderBase):
-    user_object_class = 'user'
-    group_object_class = 'group'
-    ou_object_class = 'organizationalUnit'
+    user_object_class = "user"
+    group_object_class = "group"
+    ou_object_class = "organizationalUnit"
 
     def __init__(self, config, user_attr_map=None):
         # self.user_attr_map = user_attr_map
-        self.user_attr_map = {'objectGUID': 'identifier', 'cn': 'user_name'}
-        self.root_dn = config.get('root_dn')
+        self.user_attr_map = {
+            "objectGUID": "id",
+            "sAMAccountName": "userName",
+            "displayName": "name.formatted",
+            "sn": "name.familyName",
+            "givenName": "name.givenName",
+            "employeeID": "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:employeeNumber",
+            "homePhone": 'emails[type eq "work"].value',
+            "ouID": "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:department",
+        }
+        self.root_dn = config.get("root_dn")
         self.config = config
         self._connection = None
+        self.ou_dns = {}
 
     @property
     def connection(self):
@@ -40,28 +51,48 @@ class AdUserProvider(ProviderBase):
         else:
             return self._connection
 
+    def get_ou_dns(self):
+        self.ou_dns = {}
+        elements = self.connection.extend.standard.paged_search(
+            search_base=self.root_dn,
+            search_filter="(objectCategory=organizationalUnit)",
+            search_scope=ldap3.SUBTREE,
+            paged_size=100,
+            attributes=ldap3.ALL_ATTRIBUTES,
+        )
+        for element in elements:
+            self.ou_dns[element["dn"]] = element["attributes"]
+
     def convert_ad_user_to_scim_user(self, ad_user):
-        ad_user_attrs = ad_user['attributes']
-        user = Core2EnterpriseUser()
-        for attr1, attr2 in self.user_attr_map.items():
-            ad_value = ad_user_attrs.get(attr1)
-            setattr(user, attr2, ad_value)
-        return user
+        """
+        user attr添加ouId用于识别用户属于哪个组织下
+        """
+        dn = ad_user["dn"]
+        attrs = ad_user["attributes"]
+        ou_dn = dn.split(",")[1:]
+        ou_dn = ",".join(ou_dn)
+        if not self.ou_dns:
+            self.get_ou_dns()
+        if ou_dn in self.ou_dns:
+            attrs["ouID"] = self.ou_dns[ou_dn]["objectGUID"].strip("{}")
+        else:
+            logger.warning(f"No OU find with dn{ou_dn}")
+        return get_scim_user(attrs, self.user_attr_map)
 
     def get_all_users_from_ldap(self):
         search_base = self.root_dn
-        search_filter = f'(objectClass={self.user_object_class})'
-        res = self.connection.search(
+        search_filter = f"(objectClass={self.user_object_class})"
+        res = self.connection.extend.standard.paged_search(
             search_base=search_base,
             search_filter=search_filter,
             search_scope=ldap3.SUBTREE,
+            paged_size=50,
             attributes=ldap3.ALL_ATTRIBUTES,
         )
         if not res:
             return []
         all_users = []
-        entries = json.loads(self.connection.response_to_json())['entries']
-        for item in entries:
+        for item in res:
             user = self.convert_ad_user_to_scim_user(item)
             all_users.append(user)
         return all_users
@@ -93,14 +124,14 @@ class AdUserProvider(ProviderBase):
 
     def query_async2(self, parameters, correlation_identifier):
         if parameters is None:
-            raise ArgumentNullException('parameters')
+            raise ArgumentNullException("parameters")
         if not correlation_identifier:
-            raise ArgumentNullException('correlation_identifier')
+            raise ArgumentNullException("correlation_identifier")
         if parameters.alternate_filters is None:
-            raise ArgumentException('Invalid parameters')
+            raise ArgumentException("Invalid parameters")
 
         if not parameters.schema_identifier:
-            raise ArgumentException('Invalid parameters')
+            raise ArgumentException("Invalid parameters")
 
         if not parameters.alternate_filters:
             all_users = self.get_all_users_from_ldap()
@@ -108,11 +139,11 @@ class AdUserProvider(ProviderBase):
 
         query_filter = parameters.alternate_filters[0]
         if not query_filter.attribute_path:
-            raise ArgumentException('invalid parameters')
+            raise ArgumentException("invalid parameters")
         if not query_filter.comparison_value:
-            raise ArgumentException('invalid parameters')
+            raise ArgumentException("invalid parameters")
         if query_filter.filter_operator != ComparisonOperator.Equals:
-            raise NotSupportedException('unsupported comparison operator')
+            raise NotSupportedException("unsupported comparison operator")
 
         if query_filter.attribute_path == AttributeNames.UserName:
             all_users = self.storage.users.values()
@@ -138,7 +169,7 @@ class AdUserProvider(ProviderBase):
             else:
                 return []
 
-        raise NotSupportedException('unsupported filter')
+        raise NotSupportedException("unsupported filter")
 
     def replace_async2(self, resource, correlation_identifier):
         if not resource.identifier:
@@ -158,11 +189,11 @@ class AdUserProvider(ProviderBase):
 
     def retrieve_async2(self, parameters, correlation_identifier):
         if not parameters:
-            raise ArgumentNullException('parameters')
+            raise ArgumentNullException("parameters")
         if not correlation_identifier:
-            raise ArgumentNullException('correlation_identifier')
+            raise ArgumentNullException("correlation_identifier")
         if not parameters.resource_identifier.identifier:
-            raise ArgumentNullException('parameters')
+            raise ArgumentNullException("parameters")
 
         result = None
         identifier = parameters.resource_identifier.identifier
@@ -172,13 +203,13 @@ class AdUserProvider(ProviderBase):
 
     def update_async2(self, patch, correlation_identifier):
         if not patch:
-            raise ArgumentNullException('patch')
+            raise ArgumentNullException("patch")
         if not patch.resource_identifier:
-            raise ArgumentException('Invalid patch')
+            raise ArgumentException("Invalid patch")
         if not patch.resource_identifier.identifier:
-            raise ArgumentException('invalid patch')
+            raise ArgumentException("invalid patch")
         if not patch.patch_request:
-            raise ArgumentException('invalid patch')
+            raise ArgumentException("invalid patch")
         user = self.storage.users.get(patch.resource_identifier.identifier)
         if user:
             user.apply(patch.patch_request)
