0f3ecec1f69d2e6e6951bd3c0aaee4e47b5e949a
inji-hanbin <hanbin@jinji-inc.com>
2022-04-25 19:33:39 +0800

    feat: 🎸 完成了取得openapi的权限逻辑,缺少监听事件

--- arkid/perm/custom_access.py
+++ arkid/perm/custom_access.py
@@ -1,7 +1,11 @@
 from ninja.security import HttpBearer
-
+from arkid.core.models import ExpiringToken
 
 class AuthBearer(HttpBearer):
+
     def authenticate(self, request, token):
-        if token == "supersecret":
-            return token
+        expiring_token = ExpiringToken.objects.filter(token=token).first()
+        if expiring_token:
+            if expiring_token.expired(request.tenant) is False:
+                return True
+        return False
