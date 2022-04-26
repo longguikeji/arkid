9ed34a67cde65256f08c52dcba01922444f9d4d3
官小西 <guancyxx@guancyxx.cn>
2022-04-26 04:32:23 +0000

    feat: 🎸 所有模块的页面配置

---
+++ api/v1/pages/user_manage/account_life.py
@@ -0,0 +1,66 @@
+from arkid.core import routers, pages, actions
+from arkid.core.translation import gettext_default as _
+
+tag = 'account_life'
+name = '账号生命周期'
+
+
+page = pages.TablePage(tag=tag, name=name)
+edit_page = pages.FormPage(name=_("编辑账号生命周期设置"))
+create_page = pages.FormPage(name=_("创建一个新的账号生命周期设置"))
+
+pages.register_front_pages(page)
+pages.register_front_pages(edit_page)
+pages.register_front_pages(create_page)
+
+router = routers.FrontRouter(
+    path=tag,
+    name=name,
+    page=page,
+)
+
+page.create_actions(
+    init_action=actions.DirectAction(
+        path='/api/v1/tenant/{tenant_id}/account_lifes/',
+        method=actions.FrontActionMethod.GET,
+    ),
+    global_actions=[
+        actions.CreateAction(
+            page=create_page,
+        )
+    ],
+    local_actions=[
+        actions.EditAction(
+            page=edit_page,
+        ),
+        actions.DeleteAction(
+            path="/api/v1/tenant/{tenant_id}/account_lifes/{id}/",
+        )
+    ],
+)
+
+edit_page.create_actions(
+    init_action=actions.DirectAction(
+        path='/api/v1/tenant/{tenant_id}/account_lifes/{id}/',
+        method=actions.FrontActionMethod.GET
+    ),
+    global_actions=[
+        actions.ConfirmAction(path="/api/v1/tenant/{tenant_id}/account_lifes/{id}/"),
+        actions.CancelAction(),
+        actions.ResetAction(),
+    ]
+)
+
+create_page.create_actions(
+    init_action=actions.DirectAction(
+        path='/api/v1/tenant/{tenant_id}/account_lifes/',
+        method=actions.FrontActionMethod.POST
+    ),
+    global_actions=[
+        actions.ConfirmAction(
+            path="/api/v1/tenant/{tenant_id}/account_lifes/",
+        ),
+        actions.CancelAction(),
+        actions.ResetAction(),
+    ]
+)
--- api/v1/views/user_group.py
+++ api/v1/views/user_group.py
@@ -1,6 +1,60 @@
 from arkid.core.api import api
 from arkid.core.translation import gettext_default as _
 
+@api.get("/tenant/{tenant_id}/user_groups/", tags=[_("用户分组")])
+def get_user_groups(request, tenant_id: str):
+    """ 用户分组列表,TODO
+    """
+    return []
+
+@api.get("/tenant/{tenant_id}/user_groups/{id}/", tags=[_("用户分组")])
+def get_user_group(request, tenant_id: str, id: str):
+    """ 获取用户分组,TODO
+    """
+    return {}
+
+@api.post("/tenant/{tenant_id}/user_groups/", tags=[_("用户分组")])
+def create_user_group(request, tenant_id: str):
+    """ 创建用户分组,TODO
+    """
+    return {}
+
+@api.put("/tenant/{tenant_id}/user_groups/{id}/", tags=[_("用户分组")])
+def update_user_group(request, tenant_id: str, id: str):
+    """ 编辑用户分组,TODO
+    """
+    return {}
+
+@api.delete("/tenant/{tenant_id}/user_groups/{id}/", tags=[_("用户分组")])
+def delete_user_group(request, tenant_id: str, id: str):
+    """ 删除用户分组,TODO
+    """
+    return {}
+
+@api.get("/tenant/{tenant_id}/user_groups/{user_group_id}/users/", tags=[_("用户分组")])
+def get_users_from_group(request, tenant_id: str, user_group_id: str):
+    """ 获取当前分组的用户列表,TODO
+    """
+    return {}
+
+@api.delete("/tenant/{tenant_id}/user_groups/{user_group_id}/users/{id}/", tags=[_("用户分组")])
+def remove_user_from_group(request, tenant_id: str, user_group_id: str,id:str):
+    """ 将用户移除出用户分组,TODO
+    """
+    return {}
+
+@api.post("/tenant/{tenant_id}/user_groups/{user_group_id}/users/", tags=[_("用户分组")])
+def update_users_from_group(request, tenant_id: str, user_group_id: str):
+    """ 更新当前分组的用户列表,TODO
+    """
+    return {}
+
+@api.get("/tenant/{tenant_id}/user_groups/{user_group_id}/select_users/", tags=[_("用户分组")])
+def get_select_users(request, tenant_id: str, user_group_id: str):
+    """ 获取所有用户并附加是否在当前分组的状态,TODO
+    """
+    return {}
+
 @api.get("/tenant/{tenant_id}/user_groups/{user_group_id}/permissions/",tags=[_("用户分组")])
 def get_user_group_permissions(request, tenant_id: str,user_group_id:str):
     """ 用户分组权限列表,TODO
