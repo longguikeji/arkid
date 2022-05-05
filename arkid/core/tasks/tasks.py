a52e4d26d0e7f07dbfd5c185752c741036d69552
inji-hanbin <hanbin@jinji-inc.com>
2022-05-05 15:48:04 +0800

    feat: 🎸 整了tasks的目录

--- arkid/core/tasks/tasks.py
+++ arkid/core/tasks/tasks.py
@@ -1,17 +1,46 @@
 import os, django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arkid.settings')
 django.setup()
+
 from arkid.config import get_app_config
+from arkid.extension.utils import import_extension
+from arkid.extension.models import TenantExtensionConfig, Extension
 from arkid.core.models import ApiPermission
+from arkid.common.logger import logger
+from types import SimpleNamespace
+from celery import shared_task
 from .celery import app
 import requests, uuid
+import importlib
+
+@shared_task(bind=True)
+def sync(self, config_id, *args, **kwargs):
+    try:
+        logger.info("=== arkid.core.tasks.sync start...===")
+        logger.info(f"config_id: {config_id}")
+        logger.info(f"kwargs: {kwargs}")
+        extension_config = TenantExtensionConfig.active_objects.get(id=config_id)
+        extension = extension_config.extension
+        ext_dir = extension.ext_dir
+        logger.info(f"Importing  {ext_dir}")
+        ext_name = str(ext_dir).replace('/','.')
+        ext = importlib.import_module(ext_name)
+        if ext and hasattr(ext, 'extension'):
+            ext.extension.sync(extension_config)
+            logger.info("=== arkid.core.tasks.sync end...===")
+        else:
+            logger.error(f'{ext_name} import fail')
+            return None
+    except Exception as exc:
+        max_retries = kwargs.get('max_retries', 3)
+        countdown = kwargs.get('retry_delay', 5 * 60)
+        raise self.retry(exc=exc, max_retries=max_retries, countdown=countdown)
 
 @app.task
 def update_permission():
     host = get_app_config().get_host()
     api_info = '{}/api/v1/openapi_redoc.json'.format(host)
     permission_task(None, api_info)
-
 
 def permission_task(app_temp, api_info):
     response = requests.get(api_info)
