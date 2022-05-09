09b3335d6a1fb2a38c219ed284a9d4dfbe07d5ff
wely <wely@longguikeji.com>
2022-05-09 16:44:53 +0800

    feat: 🎸 增加前端主题插件并完成部分相关接口

--- api/v1/views/front_theme.py
+++ api/v1/views/front_theme.py
@@ -1,9 +1,15 @@
+from uuid import UUID
+from django import dispatch
+from django.db.models import F
 from ninja import Schema
+from pydantic import Field
 from typing import List
 from arkid.core.api import api
 from arkid.core.translation import gettext_default as _
 from arkid.extension.models import TenantExtensionConfig, Extension
 from arkid.core.extension.front_theme import FrontThemeExtension
+from arkid.core.event import dispatch_event, Event, CREATE_FRONT_THEME_CONFIG
+from arkid.extension.utils import import_extension
 
 FrontThemeListSchemaItem = FrontThemeExtension.create_composite_config_schema(
     'FrontThemeListSchemaItem',
@@ -15,24 +21,36 @@ class FrontThemeListSchemaOut(Schema):
 def get_front_theme_list(request, tenant_id: str):
     """ 前端主题配置列表"""
     extensions = Extension.active_objects.filter(type=FrontThemeExtension.TYPE).all()
-    configs = TenantExtensionConfig.active_objects.filter(extension__in=extensions)
-    return {"data": configs}
+    configs = TenantExtensionConfig.active_objects.filter(extension__in=extensions).annotate(package=F('extension__package'))
+    return {"data": list(configs.values('package','id','name','type','config'))}
 
-FrontThemeConfigDetailOut = FrontThemeExtension.create_composite_config_schema('FrontThemeConfigDetailOut')
-class FrontThemeDetailOut(Schema):
-    data: FrontThemeConfigDetailOut
+FrontThemeConfigGetOut = FrontThemeExtension.create_composite_config_schema('FrontThemeConfigGetOut')
+class GetFrontThemeOut(Schema):
+    data: FrontThemeConfigGetOut
 
-@api.get("/tenant/{tenant_id}/front_theme/{id}/", response=FrontThemeConfigDetailOut, tags=["前端主题"],auth=None)
+@api.get("/tenant/{tenant_id}/front_theme/{id}/", response=GetFrontThemeOut, tags=["前端主题"],auth=None)
 def get_front_theme(request, tenant_id: str, id: str):
     """ 获取前端主题配置,TODO """
     config = TenantExtensionConfig.active_objects.get(id=id)
     return {"data":config}
 
-@api.post("/tenant/{tenant_id}/front_theme/", tags=["前端主题"],auth=None)
-def create_front_theme(request, tenant_id: str):
-    """ 创建前端主题配置,TODO
-    """
-    return {}
+CreateFrontThemeIn = FrontThemeExtension.create_composite_config_schema(
+    'CreateFrontThemeIn',
+    exclude=['id']
+)
+class CreateFrontThemeOut(Schema):
+    config_id: str
+
+@api.post("/tenant/{tenant_id}/front_theme/", response=CreateFrontThemeOut, tags=["前端主题"],auth=None)
+def create_front_theme(request, tenant_id: str, data:CreateFrontThemeIn):
+    """ 创建前端主题配置,TODO """
+    extension = Extension.active_objects.filter(package = data.package).first()
+    if extension:
+        ext = import_extension(extension.ext_dir)
+        tenant = request.tenant
+        config = ext.create_tenant_config(tenant, data.config.dict(), data.name, data.type)
+        return {'config_id':config.id.hex}
+    return {'error':'无法找到{data.package}对应的插件'}
 
 @api.put("/tenant/{tenant_id}/front_theme/{id}/", tags=["前端主题"],auth=None)
 def update_front_theme(request, tenant_id: str, id: str):
--- arkid/core/extension/front_theme.py
+++ arkid/core/extension/front_theme.py
@@ -1,14 +1,16 @@
 
 from django.shortcuts import render
 from ninja import Schema
+from typing import List, Optional, Literal
 from pydantic import Field
 from abc import abstractmethod
+from arkid.config import get_app_config
 from arkid.core.extension import Extension, create_extension_schema
 from arkid.core.translation import gettext_default as _
 from arkid.core import event as core_event
 from arkid.extension.models import TenantExtensionConfig
 from django.views import View
-from django.urls import re_path
+from django.urls import re_path, reverse
 from django.http.response import FileResponse
 from arkid.core.event import CREATE_FRONT_THEME_CONFIG
 
@@ -27,12 +29,10 @@ class FrontThemeExtension(Extension):
         return FrontThemeExtension.TYPE
 
     def load(self):
-        self.listen_event(CREATE_FRONT_THEME_CONFIG, self.create_front_theme_config)
+        # self.listen_event(CREATE_FRONT_THEME_CONFIG, self.create_front_theme_config)
         super().load()
 
-    def register_front_theme(self, type, schema, css_file):
-        self.register_config_schema(schema, self.package + '_' + type)
-        self.register_composite_config_schema(schema, type, exclude=['extension'])
+    def register_front_theme_by_file(self, type, css_file):
         class CSSView(View):
             def get():
                 return FileResponse(open(css_file, 'rb'))
@@ -41,17 +41,31 @@ class FrontThemeExtension(Extension):
             re_path(
                 rf'^front_theme/{self.name}/{type}/css/',
                 CSSView.as_view(),
-                name=f'{self.name}_{type}_css',
+                name=f'{type}',
             ),
         ]
-        self.register_routers(css_urls, True)
-    
-    def create_front_theme_config(self, event,  **kwargs):
-        # 保存
-        pass
+        self.register_routers(css_urls, False)
+        
+        host = get_app_config().get_frontend_host()
+        css_url = host+reverse('{self.name}:{type}')
+        self.register_front_theme(type, css_url)
+        
+    def register_front_theme(self, type, css_url):
+        
+        schema = create_extension_schema(
+            'MateriaConfigSchema',
+            self.package, 
+            base_schema = BaseFrontThemeConfigSchema,
+            fields=[
+                ('css_url', Literal[css_url], Field(title=_('CSS URL','样式地址'))),
+            ]
+        )
+        self.register_config_schema(schema, self.package + '_' + type)
+        self.register_composite_config_schema(schema, type, exclude=['extension'])
+        
         
 
 
 class BaseFrontThemeConfigSchema(Schema):
     priority: int = Field(default=1, title=_('Priority', '优先级'))
-    css_url: str = Field(title=_('CSS URL','样式地址'))
+    # css_url: str = Field(title=_('CSS URL','样式地址'))
