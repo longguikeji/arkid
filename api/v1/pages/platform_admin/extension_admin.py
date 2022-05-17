a69b26673179cbe62045864862f3fa4a920af1c3
wely <wely@longguikeji.com>
2022-05-17 18:03:14 +0800

    fix: 🐛 修改插件商店页面

--- api/v1/pages/platform_admin/extension_admin.py
+++ api/v1/pages/platform_admin/extension_admin.py
@@ -2,14 +2,18 @@ from arkid.core import routers, pages, actions
 from arkid.core.translation import gettext_default as _
 
 tag = 'extension_admin'
-name = '插件管理'
+name = '插件商店'
 
 
-page = pages.TablePage(tag=tag, name=name)
+page = pages.TabsPage(tag=tag, name=name)
+store_page = pages.TablePage(name='插件商店')
+download_page = pages.CardsPage(name='已安装')
 edit_page = pages.FormPage(name=_("编辑插件"))
 
 
 pages.register_front_pages(page)
+pages.register_front_pages(store_page)
+pages.register_front_pages(download_page)
 pages.register_front_pages(edit_page)
 
 
@@ -19,7 +23,33 @@ router = routers.FrontRouter(
     page=page,
 )
 
-page.create_actions(
+page.add_pages([
+    store_page,
+    # download_page
+])
+
+store_page.create_actions(
+    init_action=actions.DirectAction(
+        path='/api/v1/extensions/',
+        method=actions.FrontActionMethod.GET,
+    ),
+    global_actions={
+        'create': actions.CreateAction(
+            path='/api/v1/extensions/',
+        )
+    },
+    local_actions={
+        # 加载/卸载 插件 TODO
+        "edit": actions.EditAction(
+            page=edit_page,
+        ),
+        "delete": actions.DeleteAction(
+            path="/api/v1/extensions/{id}/",
+        )
+    },
+)
+
+download_page.create_actions(
     init_action=actions.DirectAction(
         path='/api/v1/extensions/',
         method=actions.FrontActionMethod.GET,
