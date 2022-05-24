828de0b5ea584648119bef3b96a9874923ef9033
官小西 <guancyxx@guancyxx.cn>
2022-05-24 10:35:37 +0800

    feat: 🎸 租户接口expand_objects

---
+++ api/v1/schema/tenant.py
@@ -0,0 +1,94 @@
+from typing import List
+from ninja import Field, ModelSchema, Schema
+
+from arkid.core.models import Tenant
+from arkid.core.schema import ResponseSchema
+
+
+class TenantListQueryIn(Schema):
+    name__contains:str = Field(
+        default="",
+        title=_("租户名称")
+    )
+        
+class TenantListItemOut(ModelSchema):
+    class Config:
+        model = Tenant
+        model_fields = ["id","name", "slug", "icon"]
+        
+class TenantListOut(ResponseSchema):
+    data: List[TenantListItemOut]
+    
+class TenantItemOut(ModelSchema):
+
+    class Config:
+        model = Tenant
+        model_fields = ["name","slug","icon"]
+        
+class TenantOut(ResponseSchema):
+    
+    data: TenantItemOut
+    
+class TenantCreateIn(ModelSchema):
+    class Config:
+        model = Tenant
+        model_fields = ["name","slug","icon"]
+
+class TenantCreateOut(ResponseSchema):
+    pass
+
+class TenantUpdateIn(ModelSchema):
+    class Config:
+        model = Tenant
+        model_fields = ["name"]
+        
+class TenantUpdateOut(Schema):
+    pass
+
+class TenantDeleteOut(ResponseSchema):
+    pass
+
+class TenantConfigItemOut(Schema):
+    id: str = Field(
+        readonly = True
+    )
+    
+    name: str = Field(
+        title=_("租户名称"),
+    )
+    
+    slug: str = Field(
+        title=_("slug")
+    )
+    
+    icon: str = Field(
+        title=_("图标")
+    )
+
+class TenantConfigOut(ResponseSchema):
+    data: TenantConfigItemOut
+    
+class TenantConfigUpdateIn(Schema):
+    name: str = Field(
+        title=_("租户名称"),
+    )
+    
+    slug: str = Field(
+        title=_("slug")
+    )
+    
+    icon: str = Field(
+        title=_("图标")
+    )
+        
+class TenantConfigUpdateOut(ResponseSchema):
+    pass
+
+class DefaultTenantItemOut(ModelSchema):
+    
+    class Config:
+        model = Tenant
+        model_fields = ["id", "name"]
+
+class DefaultTenantOut(ResponseSchema):
+    data: DefaultTenantItemOut
--- api/v1/views/tenant.py
+++ api/v1/views/tenant.py
@@ -1,26 +1,14 @@
 from re import T
 from typing import List
 from uuid import UUID
+from django.shortcuts import get_object_or_404
 from ninja import Field, File, ModelSchema, Query, Schema
 from arkid.core.api import api, operation
 from arkid.core.models import Tenant
 from arkid.core.translation import gettext_default as _
 from arkid.core.schema import ResponseSchema
 from arkid.core.error import ErrorCode
-
-class TenantListQueryIn(Schema):
-    name__contains:str = Field(
-        default="",
-        title=_("租户名称")
-    )
-        
-class TenantListItemOut(ModelSchema):
-    class Config:
-        model = Tenant
-        model_fields = ["id","name", "slug", "icon"]
-        
-class TenantListOut(ResponseSchema):
-    data: List[TenantListItemOut]
+from api.v1.schema.tenant import *
 
 @api.get("/tenants/", response=TenantListOut,tags=["租户管理"],auth=None)
 # @operation(List[TenantListOut])
@@ -28,7 +16,7 @@ def get_tenant_list(request, query_data:TenantListQueryIn=Query(...)):
     """ 获取租户列表
     """
 
-    tenants = Tenant.active_objects
+    tenants = Tenant.expand_objects
     query_data = query_data.dict()
     if query_data:
         tenants = tenants.filter(**query_data)
@@ -36,16 +24,6 @@ def get_tenant_list(request, query_data:TenantListQueryIn=Query(...)):
     return {
         "data": list(tenants.all())
     }
-
-class TenantItemOut(ModelSchema):
-
-    class Config:
-        model = Tenant
-        model_fields = ["name","slug","icon"]
-        
-class TenantOut(ResponseSchema):
-    
-    data: TenantItemOut
 
 @api.get("/tenants/{id}/", response=TenantOut,tags=["租户管理"],auth=None)
 @operation(TenantOut)
@@ -61,42 +39,24 @@ def get_tenant(request, id: str):
         }
     }
 
-class TenantCreateIn(ModelSchema):
-    class Config:
-        model = Tenant
-        model_fields = ["name","slug","icon"]
-
-class TenantCreateOut(ResponseSchema):
-    pass
-
 @api.post("/tenants/",response=TenantCreateOut,tags=["租户管理"],auth=None)
 def create_tenant(request, data:TenantCreateIn):
     """ 创建租户
     """
 
-    tenant = Tenant.valid_objects.create(**data.dict())
+    tenant = Tenant.expand_objects.create(**data.dict())
     return {'error': ErrorCode.OK.value}
-class TenantUpdateIn(ModelSchema):
-    class Config:
-        model = Tenant
-        model_fields = ["name"]
-        
-class TenantUpdateOut(Schema):
-    pass
 
 @api.post("/tenants/{id}/", response=TenantUpdateOut,tags=["租户管理"],auth=None)
 @operation(TenantUpdateOut)
 def update_tenant(request, id: str, data:TenantUpdateIn):
     """ 编辑租户
     """
-    tenant = Tenant.active_objects.get(id=id)
-    tenant.name = data.dict().get("name")
+    tenant = get_object_or_404(Tenant.expand_objects,id=id)
+    for attr, value in data.dict().items():
+        setattr(tenant, attr, value)
     tenant.save()
     return {'error': ErrorCode.OK.value}
-
-
-class TenantDeleteOut(ResponseSchema):
-    pass
 
 @api.delete("/tenants/{id}/", response=TenantDeleteOut, tags=["租户管理"],auth=None)
 @operation(TenantDeleteOut)
@@ -106,26 +66,6 @@ def delete_tenant(request, id: str):
     tenant = Tenant.active_objects.get(id=id)
     tenant.delete()
     return {'error': ErrorCode.OK.value}
-        
-class TenantConfigItemOut(Schema):
-    id: str = Field(
-        readonly = True
-    )
-    
-    name: str = Field(
-        title=_("租户名称"),
-    )
-    
-    slug: str = Field(
-        title=_("slug")
-    )
-    
-    icon: str = Field(
-        title=_("图标")
-    )
-
-class TenantConfigOut(ResponseSchema):
-    data: TenantConfigItemOut
 
 @api.get("/tenants/{tenant_id}/config/", response=TenantConfigOut, tags=["租户管理"],auth=None)
 @operation(TenantConfigOut)
@@ -143,38 +83,12 @@ def get_tenant_config(request, tenant_id: str):
         }
     }
 
-
-class TenantConfigUpdateIn(Schema):
-    name: str = Field(
-        title=_("租户名称"),
-    )
-    
-    slug: str = Field(
-        title=_("slug")
-    )
-    
-    icon: str = Field(
-        title=_("图标")
-    )
-        
-class TenantConfigUpdateOut(ResponseSchema):
-    pass
-
 @api.post("/tenants/{tenant_id}/config/", response=TenantConfigUpdateOut,tags=["租户管理"],auth=None)
 @operation(TenantConfigUpdateOut)
 def update_tenant_config(request, tenant_id: str,data:TenantConfigUpdateIn):
-    """ 编辑租户配置,TODO
+    """ 编辑租户配置
     """
     return {'error': ErrorCode.OK.value}
-
-class DefaultTenantItemOut(ModelSchema):
-    
-    class Config:
-        model = Tenant
-        model_fields = ["id", "name"]
-
-class DefaultTenantOut(ResponseSchema):
-    data: DefaultTenantItemOut
 
 @api.get("/default_tenant/",response=DefaultTenantOut, tags=["租户管理"], auth=None)
 def default_tenant(request):
