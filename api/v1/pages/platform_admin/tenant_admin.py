c465e8110afdbde8cb59420e640bbaef5f062494
官小西 <guancyxx@guancyxx.cn>
2022-05-25 09:28:15 +0800

    feat: 🎸 租户列表:去掉增删改操作,添加切换操作

--- api/v1/pages/platform_admin/tenant_admin.py
+++ api/v1/pages/platform_admin/tenant_admin.py
@@ -6,12 +6,8 @@ tag = 'tenant_admin'
 name = '租户列表'
 
 page = pages.TablePage(name=name,tag=tag)
-edit_page = pages.FormPage(name=_("编辑租户"))
-
 
 pages.register_front_pages(page)
-pages.register_front_pages(edit_page)
-
 
 router = routers.FrontRouter(
     path=tag,
@@ -26,31 +22,11 @@ page.create_actions(
         path='/api/v1/tenants/',
         method=actions.FrontActionMethod.GET,
     ),
-    global_actions={
-        'create': actions.CreateAction(
-            path='/api/v1/tenants/',
-        )
-    },
     local_actions={
-        "edit": actions.EditAction(
-            page=edit_page,
-        ),
-        "delete":actions.DeleteAction(
-            path="/api/v1/tenants/{id}/",
+        "switch": actions.URLAction(
+            name=_("切换"),
+            path='/api/v1/mine/switch_tenant/{id}/',
+            method=actions.FrontActionMethod.GET
         )
     },
 )
-
-edit_page.create_actions(
-    init_action=actions.DirectAction(
-        path='/api/v1/tenants/{id}/',
-        method=actions.FrontActionMethod.GET
-    ),
-    global_actions={
-       'confirm': actions.ConfirmAction(
-            path="/api/v1/tenants/{id}/"
-        ),
-    }
-)
-
-
