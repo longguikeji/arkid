b68247f137a9f37f0b94594281f72fde6acba525
官小西 <guancyxx@guancyxx.cn>
2022-06-27 08:14:59 +0800

    feat: 🎸 多语言包插件：简体中文处理无用代码以及文档

--- extension_root/com_longgui_language_zh/__init__.py
+++ extension_root/com_longgui_language_zh/__init__.py
@@ -1,15 +1,6 @@
-import uuid
-import json
-import logging
-from .constants import KEY
-from arkid import core
-from arkid.core import extension, event
 from arkid.core.extension.language import LanguageExtension
 from arkid.core.translation import gettext_default as _
-from django.conf import settings
-import os
 
-    
 class TranslationZhExtension(LanguageExtension):
     def language_type(self) -> str:
         return _("简体中文")
---
+++ extension_root/com_longgui_language_zh/index.md
@@ -0,0 +1,13 @@
+# 多语言包插件：简体中文
+
+
+
+## 抽象方法实现
+* [language_type](#extension_root.com_longgui_language_zh.TranslationZhExtension.language_type)
+* [language_data](#extension_root.com_longgui_sms_aliyun.TranslationZhExtension.language_data)
+
+## 代码
+
+::: extension_root.com_longgui_language_zh.TranslationZhExtension
+    rendering:
+        show_source: true
