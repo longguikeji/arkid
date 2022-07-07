ff68607225e10a98cdf73a3320a55c794cfab088
inji-hanbin <hanbin@jinji-inc.com>
2022-07-07 18:32:05 +0800

    feat: 🎸 更新了pages的定义

--- extension_root/com_longgui_impower_rule/schema.py
+++ extension_root/com_longgui_impower_rule/schema.py
@@ -1,12 +1,14 @@
 from uuid import UUID
+from enum import Enum
 from ninja import Field
 from typing import List, Optional
 from ninja import ModelSchema, Schema
 from arkid.core.translation import gettext_default as _
 from arkid.core.schema import ResponseSchema
 from arkid.core import pages, actions
+from arkid.core.extension import create_extension_page
 
-app_page = pages.TablePage(select=True, name='应用列表')
+app_page = create_extension_page(__file__,pages.TablePage, select=True, name='应用列表')
 app_page.create_actions(
     init_action=actions.DirectAction(
         path='/api/v1/tenant/{tenant_id}/all_apps_in_arkid/',
@@ -14,7 +16,7 @@ app_page.create_actions(
     )
 )
 
-app_permission_page = pages.TablePage(name=_("该应用权限"), select=True)
+app_permission_page = create_extension_page(__file__,pages.TablePage, select=True, name='该应用权限')
 app_permission_page.create_actions(
     init_action=actions.DirectAction(
         path='/api/v1/tenant/{tenant_id}/permissions?app_id={app_id}',
@@ -30,10 +32,28 @@ class ImpowerRulePermissionCreateInItem(Schema):
     id:UUID = Field(hidden=True)
     name:str
 
+class MatchField(str, Enum):
+    username = 'username'
+    age = 'age'
+    gender = 'gender'
+
+class MatchSymbol(str, Enum):
+    等于 = '等于'
+    大于 = '大于'
+    小于 = '小于'
+
 class ImpowerRuleCreateIn(Schema):
 
-    regular: str = Field(
-        title=_("规则")
+    matchfield: MatchField = Field(
+        title=_("选择匹配字段"),
+        default='username'
+    )
+    matchsymbol: MatchSymbol = Field(
+        title=_("选择匹配符号"),
+        default='等于'
+    )
+    matchvalue: str = Field(
+        title=_("输入匹配值"),
     )
 
     app: Optional[ImpowerRuleAppCreateInItem] = Field(
