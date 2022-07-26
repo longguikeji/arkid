4711b9bd5ed7b0b6e413de1a5fb80b4c4de29e8c
luolu <louis.law@hotmail.com>
2022-07-26 08:33:35 +0000

    feat: 🎸 add arkstore app

--- api/v1/pages/app_manage/app_list.py
+++ api/v1/pages/app_manage/app_list.py
@@ -5,28 +5,35 @@ tag = 'app_list'
 name = '应用列表'
 
 
-page = pages.TablePage(tag=tag, name=name)
+page = pages.TabsPage(tag=tag, name=name)
+
 edit_page = pages.FormPage(name=_("编辑应用"))
 config_page = pages.FormPage(name=_("配置应用"))
 openapi_page = pages.FormPage(name=_("开放API配置"))
 appstore_page = pages.TabsPage(name=_("APP Store", "应用商店"))
-app_list_page = pages.TablePage(name=_("APP Store", "应用商店"))
-app_purchased_page = pages.TablePage(name=_("Purchased", "已购买"))
-order_page = pages.FormPage(name=_('Order', '购买'))
+store_page = pages.CardsPage(name=_("APP Store", "应用商店"))
+app_purchased_page = pages.CardsPage(name=_("Purchased", "已安装"))
+order_page = pages.StepPage(name=_('Order', '购买'))
+trial_page = pages.FormPage(name=_('Trial', '试用'))
+installed_page = pages.CardsPage(name='本地应用')
+price_page = pages.CardsPage(name='选择价格')
+copies_page = pages.FormPage(name='人天份数')
+payment_page = pages.FormPage(name='支付')
 
 pages.register_front_pages(page)
+pages.register_front_pages(installed_page)
 pages.register_front_pages(edit_page)
 pages.register_front_pages(config_page)
 pages.register_front_pages(openapi_page)
 pages.register_front_pages(appstore_page)
-pages.register_front_pages(app_list_page)
+pages.register_front_pages(store_page)
 pages.register_front_pages(app_purchased_page)
 pages.register_front_pages(order_page)
+pages.register_front_pages(trial_page)
+pages.register_front_pages(price_page)
+pages.register_front_pages(copies_page)
+pages.register_front_pages(payment_page)
 
-appstore_page.add_pages([
-    app_list_page,
-    app_purchased_page
-])
 
 router = routers.FrontRouter(
     path=tag,
@@ -35,7 +42,13 @@ router = routers.FrontRouter(
     page=page,
 )
 
-page.create_actions(
+page.add_pages([
+    installed_page,
+    store_page,
+    app_purchased_page
+])
+
+installed_page.create_actions(
     init_action=actions.DirectAction(
         path='/api/v1/tenant/{tenant_id}/apps/',
         method=actions.FrontActionMethod.GET,
@@ -44,10 +57,6 @@ page.create_actions(
         'create':actions.CreateAction(
             path='/api/v1/tenant/{tenant_id}/apps/'
         ),
-        'appstore':actions.OpenAction(
-            name='应用商店',
-            page=appstore_page
-        )
     },
     local_actions={
         "config":actions.OpenAction(
@@ -104,15 +113,16 @@ config_page.create_actions(
     }
 )
 
-app_list_page.create_actions(
+store_page.create_actions(
     init_action=actions.DirectAction(
         path='/api/v1/tenant/{tenant_id}/arkstore/apps/',
         method=actions.FrontActionMethod.GET
     ),
     local_actions={
-        "order": actions.OpenAction(
-            name='购买',
-            page=order_page
+        "order": actions.DirectAction(
+            name='安装到本地',
+            path='/api/v1/tenant/{tenant_id}/arkstore/install/{uuid}/',
+            method=actions.FrontActionMethod.POST
         )
     },
 )
@@ -123,23 +133,73 @@ app_purchased_page.create_actions(
         method=actions.FrontActionMethod.GET
     ),
     local_actions={
-        "install": actions.DirectAction(
-            path="/api/v1/tenant/{tenant_id}/install/{uuid}/",
-            method=actions.FrontActionMethod.POST
+        "order": actions.OpenAction(
+            name='购买',
+            page=order_page
+        ),
+        "trial": actions.OpenAction(
+            name='试用',
+            page=trial_page
         )
     },
 )
 
-order_page.create_actions(
+order_page.add_pages([
+    price_page,
+    copies_page,
+    payment_page
+])
+
+price_page.create_actions(
     init_action=actions.DirectAction(
         path='/api/v1/tenant/{tenant_id}/arkstore/order/extensions/{uuid}/',
+        method=actions.FrontActionMethod.GET
+    ),
+    local_actions={
+       'next': actions.NextAction(
+            name="选择价格"
+        ),
+    }
+)
+
+copies_page.create_actions(
+    init_action=actions.DirectAction(
+        path="/api/v1/tenant/{tenant_id}/arkstore/order/extensions/{uuid}/set_copies/",
+        method=actions.FrontActionMethod.POST
+    ),
+    global_actions={
+       'next': actions.NextAction(
+            name="创建订单",
+            path="/api/v1/tenant/{tenant_id}/arkstore/order/extensions/{uuid}/",
+            method=actions.FrontActionMethod.POST
+        ),
+    }
+)
+
+payment_page.create_actions(
+    init_action=actions.DirectAction(
+        path="/api/v1/tenant/{tenant_id}/arkstore/order/{order_no}/payment/",
+        method=actions.FrontActionMethod.GET
+    ),
+    global_actions={
+       'next': actions.NextAction(
+            name="已支付",
+            path="/api/v1/tenant/{tenant_id}/arkstore/purchase/order/{order_no}/payment_status/",
+            method=actions.FrontActionMethod.GET
+        ),
+    }
+)
+
+trial_page.create_actions(
+    init_action=actions.DirectAction(
+        path='/api/v1/tenant/{tenant_id}/arkstore/trial/extensions/{uuid}/',
         method=actions.FrontActionMethod.GET,
     ),
     global_actions={
-        "payed": actions.DirectAction(
-            name='已支付',
-            path='/api/v1/tenant/{tenant_id}/arkstore/order/status/extensions/{uuid}/',
-            method=actions.FrontActionMethod.GET
+        "confirm": actions.DirectAction(
+            name='试用',
+            path='/api/v1/tenant/{tenant_id}/arkstore/trial/extensions/{uuid}/',
+            method=actions.FrontActionMethod.POST
         ),
     },
 )
