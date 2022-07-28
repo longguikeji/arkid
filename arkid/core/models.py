548c37423fc565667e1658e0d5efcdcbdae7fbdf
inji-hanbin <hanbin@jinji-inc.com>
2022-07-28 19:52:30 +0800

    feat: 🎸 代码编辑错误

--- arkid/core/models.py
+++ arkid/core/models.py
@@ -410,52 +410,6 @@ class UserPermissionResult(BaseModel, ExpandModel):
         return f'User: {self.user.username}'
 
 
-
-class UserPermissionResult(BaseModel, ExpandModel):
-    class Meta(object):
-        verbose_name = _("UserPermissionResult", "用户权限结果")
-        verbose_name_plural = _("UserPermissionResult", "用户权限结果")
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='用户')
-    tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT, verbose_name='租户')
-    app = models.ForeignKey(
-        App,
-        default=None,
-        null=True,
-        blank=True,
-        on_delete=models.PROTECT,
-        verbose_name='App',
-    )
-    result = models.CharField(
-        max_length=1024, blank=True, null=True, verbose_name='权限结果'
-    )
-
-    def __str__(self) -> str:
-        return f'User: {self.user.username}'
-
-class GroupPermissionResult(BaseModel, ExpandModel):
-    class Meta(object):
-        verbose_name = _("GroupPermissionResult", "分组权限结果")
-        verbose_name_plural = _("GroupPermissionResult", "分组权限结果")
-
-    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE, verbose_name='用户分组')
-    tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT, verbose_name='租户')
-    app = models.ForeignKey(
-        App,
-        default=None,
-        null=True,
-        blank=True,
-        on_delete=models.PROTECT,
-        verbose_name='App',
-    )
-    result = models.CharField(
-        max_length=1024, blank=True, null=True, verbose_name='权限结果'
-    )
-
-    def __str__(self) -> str:
-        return f'User: {self.user_group.name}'
-
-
 class GroupPermissionResult(BaseModel, ExpandModel):
     class Meta(object):
         verbose_name = _("GroupPermissionResult", "分组权限结果")
