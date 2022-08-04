cda437b447ad1007166b8e97c022eac9e56e51b1
官小西 <guancyxx@guancyxx.cn>
2022-08-04 23:09:55 +0800

    feat: 🎸 添加arkid访问地址平台设置

--- api/v1/views/platform_config.py
+++ api/v1/views/platform_config.py
@@ -3,7 +3,7 @@ from arkid.core.constants import *
 from arkid.core.translation import gettext_default as _
 from api.v1.schema.platform_config import *
 from arkid.core.models import Platform
-from arkid.core.error import ErrorCode, ErrorDict
+from arkid.core.error import ErrorCode, ErrorDict, SuccessDict
 
 @api.get("/platform_config/",response=PlatformConfigOut, tags=["平台配置"])
 @operation(roles=[PLATFORM_ADMIN])
@@ -16,6 +16,29 @@ def get_platform_config(request):
 @operation(roles=[PLATFORM_ADMIN])
 def update_platform_config(request,data:PlatformConfigIn):
     """ 更新平台配置,TODO
+    """
+    config = Platform.get_config()
+    for key,value in data.dict().items():
+        setattr(config,key,value)
+    config.save()
+    
+    return {"error": ErrorCode.OK.value}
+
+@api.get("/frontend_url/",response=FrontendUrlOut, tags=["平台配置"],auth=None)
+def get_frontend_url(request):
+    """ 获取ArkId访问地址
+    """
+    config = Platform.get_config()
+    return SuccessDict(
+        data={
+            "url": config.get("frontend_url","")
+        }
+    )
+
+
+@api.post("/frontend_url/",response=FrontendUrlOut, tags=["平台配置"],auth=None)
+def set_frontend_url(request,data:FrontendUrlSchema):
+    """ 获取ArkId访问地址
     """
     config = Platform.get_config()
     for key,value in data.dict().items():
