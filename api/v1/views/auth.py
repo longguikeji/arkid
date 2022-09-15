08c003d33c1310ff539de5439523d0e1a9594e75
fanhe <fanhe@longguikeji.com>
2022-09-15 12:32:50 +0800

    fix: 🐛 修改认证之后cookie写入方式

--- api/v1/views/auth.py
+++ api/v1/views/auth.py
@@ -1,14 +1,15 @@
+from email import header
 from arkid.core.event import register_event, dispatch_event, Event
 from arkid.core.api import api, operation
 from arkid.core.translation import gettext_default as _
 from arkid.core.token import refresh_token
 from arkid.core.error import ErrorCode, ErrorDict
 from api.v1.schema.auth import *
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 @api.post("/tenant/{tenant_id}/auth/", response=AuthOut, tags=['登录与注册'], auth=None)
 @operation(AuthOut, use_id=True)
-def auth(request, tenant_id: str, event_tag: str, data: AuthIn, response: HttpResponse):
+def auth(request, tenant_id: str, event_tag: str, data: AuthIn):
     tenant = request.tenant
     request_id = request.META.get('request_id')
 
@@ -23,8 +24,9 @@ def auth(request, tenant_id: str, event_tag: str, data: AuthIn, response: HttpRe
     token = refresh_token(user)
     netloc = request.get_host().split(':')[0]
     domain = ('.'.join(netloc.split('.')[-2:]))
-    response.set_cookie("token", token, domain="."+domain, httponly=True)
-    return {'error': ErrorCode.OK.value, 'data': {'user': user, 'token': token}}
+    response = JsonResponse({'error': ErrorCode.OK.value, 'data': {'user': {"id": user.id.hex, "username": user.username}, 'token': token}})
+    response.set_cookie("arkid_token", token, domain=domain, httponly=True)
+    return response
 
 @api.post("/tenant/{tenant_id}/reset_password/", response=ResetPasswordOut, tags=['登录与注册'],auth=None)
 @operation(ResetPasswordOut, use_id=True)
