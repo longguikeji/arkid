11820cc7611967cf6924aeca7ebfc604a350d58d
luolu <louis.law@hotmail.com>
2022-11-11 07:34:42 +0000

    fix: 🐛 修复migrations冲突

--- arkid/core/migrations/0035_privateapp_values_data.py
+++ arkid/core/migrations/0035_privateapp_values_data.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0033_privateapp'),
+        ('core', '0034_privateapp'),
     ]
 
     operations = [
