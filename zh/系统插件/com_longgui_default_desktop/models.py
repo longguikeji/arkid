85d17475f890a1864e4f509fad2346076f26d21d
guancyxx <jiashuai@longguikeji.com>
2023-02-20 01:36:59 +0000

    guancyxx published a site update

--- zh/系统插件/com_longgui_default_desktop/models.py
+++ zh/系统插件/com_longgui_default_desktop/models.py
@@ -1,18 +1,25 @@
 from email.policy import default
 from django.db import models
+from arkid.common.model import BaseModel
 from arkid.core.translation import gettext_default as _
+from arkid.core.models import Tenant
 from django.apps import AppConfig
-from arkid.core.models import TenantExpandAbstract
 
 app_label = "com_longgui_default_desktop"
 
 class LongguiDefaultDesktopAppConfig(AppConfig):
     name = app_label
     
-class DefaultDesktop(TenantExpandAbstract):
+class DefaultDesktop(BaseModel):
     class Meta:
         app_label = app_label
     
     default_desktop = models.CharField(verbose_name=_('Default Desktop', '默认桌面'),blank=True,null=True, max_length=1024,default="/desktop/")
     
-
+    target = models.OneToOneField(
+        Tenant,
+        blank=True,
+        default=None,
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)s",
+    )
--- 系统插件/com_longgui_default_desktop/__init__.py
+++ 系统插件/com_longgui_default_desktop/__init__.py
@@ -1,3 +1,4 @@
+import json
 from arkid.core import extension
 from arkid.core.translation import gettext_default as _
 from .models import DefaultDesktop
@@ -9,7 +10,6 @@ class DefaultDesktopExtension(extension.Extension):
 
     def load(self):
         super().load()
-        self.register_extend_field(DefaultDesktop, 'default_desktop')
         from api.v1.schema.tenant import TenantConfigItemOut, TenantConfigUpdateIn, TenantItemOut, DefaultTenantItemOut
         from api.v1.views.loginpage import LoginPageTenantSchema
         self.register_extend_api(
@@ -21,6 +21,29 @@ class DefaultDesktopExtension(extension.Extension):
             default_desktop=(Optional[str], Field(
                 title=_("默认桌面路径")))
         )
+
+        self.listen_event("api_v1_views_tenant_get_tenant", self.get_default_desktop)
+        self.listen_event("api_v1_views_tenant_get_tenant_config", self.get_default_desktop)
+        self.listen_event("api_v1_views_tenant_default_tenant", self.get_default_desktop)
+
+        self.listen_event("api_v1_views_loginpage_login_page", self.login_page_event)
+
+        self.listen_event("api_v1_views_tenant_update_tenant_config",self.update_tenant_config_event)
+
+    
+    def get_default_desktop(self,event,*argc, **kwargs):
+        default_desktop,_ = DefaultDesktop.active_objects.get_or_create(target=event.tenant)
+        event.response["data"]["default_desktop"] = default_desktop.default_desktop
+
+    def login_page_event(self,event,*argc, **kwargs):
+        default_desktop,_ = DefaultDesktop.active_objects.get_or_create(target=event.tenant)
+        event.response["tenant"]["default_desktop"] = default_desktop.default_desktop
+
+    def update_tenant_config_event(self,event,*argc, **kwargs):
+        data = event.request.POST or json.load(event.request.body)
+        default_desktop,_ = DefaultDesktop.active_objects.get_or_create(target=event.tenant)
+        default_desktop.default_desktop = data.get("default_desktop",default_desktop.default_desktop)
+        default_desktop.save()
 
 
 extension = DefaultDesktopExtension()
